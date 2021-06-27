ab72d838a6660b2630399961eb0f20b6ff56f834
sunruoshi <ruoshisun@gmail.com>
2021-06-27 10:59:54 +0800

    [add] add P1305.cpp

---
+++ [数据结构1-2] 二叉树/P1305-新二叉树.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 26;
+
+struct Node {
+    int left, right;
+} node[MAXN];
+
+void preOrder(int root) {
+    if (root == -1) return;
+    printf("%c", root + 'a');
+    preOrder(node[root].left);
+    preOrder(node[root].right);
+}
+
+int main() {
+    int n, root;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        char s[3];
+        scanf("%s", s);
+        if (i == 0) root = s[0] - 'a';
+        node[s[0] - 'a'].left = s[1] == '*' ? -1 : s[1] - 'a';
+        node[s[0] - 'a'].right = s[2] == '*' ? -1 : s[2] - 'a';
+    }
+    preOrder(root);
+    return 0;
+}
