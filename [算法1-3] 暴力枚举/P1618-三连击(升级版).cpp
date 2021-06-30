9f481ea24d74633f5b4b221abb608d9039cc81ba
sunruoshi <ruoshisun@gmail.com>
2021-06-30 21:00:24 +0800

    [refactor] refactor P1157 code

--- [算法1-3] 暴力枚举/P1618-三连击(升级版).cpp
+++ [算法1-3] 暴力枚举/P1618-三连击(升级版).cpp
@@ -1,10 +1,9 @@
-#include <cstdio>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int a, b, c, cnt;
+int a, b, c, p, cnt, temp[10], visited[10];
 
-void dfs(int start, vector<int>& temp, vector<int>& visited) {
+void dfs(int start) {
     if (start == 10) {
         int n1 = 100 * temp[0] + 10 * temp[1] + temp[2];
         int n2 = 100 * temp[3] + 10 * temp[4] + temp[5];
@@ -16,19 +15,18 @@ void dfs(int start, vector<int>& temp, vector<int>& visited) {
     }
     for (int i = 1; i <= 9; i++) {
         if (!visited[i]) {
-            temp.push_back(i);
+            temp[p++] = i;
             visited[i] = 1;
-            dfs(start + 1, temp, visited);
+            dfs(start + 1);
             visited[i] = 0;
-            temp.pop_back();
+            temp[p--] = 0;
         }
     }
 }
 
 int main() {
     scanf("%d %d %d", &a, &b, &c);
-    vector<int> temp, visited (10, 0);
-    dfs(1, temp, visited);
+    dfs(1);
     if (!cnt) printf("No!!!");
     return 0;
 }
