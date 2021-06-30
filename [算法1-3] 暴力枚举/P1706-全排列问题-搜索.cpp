b31b852048387e3b768f39d49feed9c569365418
sunruoshi <ruoshisun@gmail.com>
2021-07-01 04:07:30 +0800

    [refactor] refactor P1706 dfs code

--- [算法1-3] 暴力枚举/P1706-全排列问题-搜索.cpp
+++ [算法1-3] 暴力枚举/P1706-全排列问题-搜索.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 int n, p, temp[10], visited[10];
 
-void dfs(int len, int start) {
-    if (start == len + 1) {
+void dfs(int start) {
+    if (start == n + 1) {
         for (int i = 0; i < n; i++) {
             printf("%5d", temp[i]);
         }
         printf("\n");
         return;
     }
-    for (int i = 1; i <= len; i++) {
+    for (int i = 1; i <= n; i++) {
         if (!visited[i]) {
             temp[p++] = i;
             visited[i] = 1;
-            dfs(len, start + 1);
+            dfs(start + 1);
             visited[i] = 0;
             temp[p--] = 0;
         }
@@ -24,6 +24,6 @@ void dfs(int len, int start) {
 
 int main() {
     scanf("%d", &n);
-    dfs(n, 1);
+    dfs(1);
     return 0;
 }
