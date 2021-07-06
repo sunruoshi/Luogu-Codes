c75482de59e4a8a786e886c5af221e984d949b5b
sunruoshi <ruoshisun@gmail.com>
2021-07-06 18:05:40 +0800

    [refactor] refactor P4387 code

--- [数据结构1-1] 线性表/P4387-[深基15.习9]-验证栈序列.cpp
+++ [数据结构1-1] 线性表/P4387-[深基15.习9]-验证栈序列.cpp
@@ -1,34 +1,29 @@
-#include <iostream>
-#include <stack>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-	int q, n;
-	cin >> q;
-	while (q--) {
-		stack<int> myStack;
-		cin >> n;
-		int pushed[n], poped[n], pos = 0;
-		for (int i = 0; i < n; i++) {
-			cin >> pushed[i];
-		}
-		for (int i = 0; i < n; i++) {
-			cin >> poped[i];
-		}
-		for (int i = 0; i < n; i++) {
-			myStack.push(pushed[i]);
-			while (myStack.top() == poped[pos]) {
-				myStack.pop();
-				pos++;
-				if (myStack.empty())
-					break;
-			}
-		}
-		if (myStack.empty()) {
-			cout << "Yes" << endl;
-		} else {
-			cout << "No" << endl;
-		}
-	}
-	return 0;
+    int q, n;
+    cin >> q;
+    while (q--) {
+        stack<int> s;
+        cin >> n;
+        int pushed[n], poped[n], pos = 0;
+        for (int i = 0; i < n; i++) {
+            cin >> pushed[i];
+        }
+        for (int i = 0; i < n; i++) {
+            cin >> poped[i];
+        }
+        for (int i = 0; i < n; i++) {
+            s.push(pushed[i]);
+            while (s.top() == poped[pos]) {
+                s.pop();
+                pos++;
+                if (s.empty()) break;
+            }
+        }
+        string ans = s.empty() ? "Yes" : "No";
+        cout << ans << endl;
+    }
+    return 0;
 }
