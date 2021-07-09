8c642094f9a093f2ca6d868256cdcfeda5c527f5
sunruoshi <ruoshisun@gmail.com>
2021-07-09 08:57:10 +0800

    [refactor] refactor P7072 code

--- [CSP入门组]/P7072-[CSP-J2020]-直播获奖.cpp
+++ [CSP入门组]/P7072-[CSP-J2020]-直播获奖.cpp
@@ -1,17 +1,15 @@
-#include <cstdio>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     int n, w;
     scanf("%d %d", &n, &w);
-    vector<int> myList;
+    vector<int> vec;
     for (int i = 1; i <= n; i++) {
         int d, cur = max(1, i * w / 100);
         scanf("%d", &d);
-        myList.insert(lower_bound(myList.begin(), myList.end(), d), d);
-        printf("%d ", myList[myList.size() - cur]);
+        vec.insert(lower_bound(vec.begin(), vec.end(), d), d);
+        printf("%d ", vec[vec.size() - cur]);
     }
     return 0;
 }
