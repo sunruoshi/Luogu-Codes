0cb89bd8b5912657c90f6f8d82ad1360ec733d06
sunruoshi <ruoshisun@gmail.com>
2021-07-16 11:43:39 +0800

    [refactor] refactor P1601 code

--- [算法1-1] 模拟与高精度/P1601-A+B Problem（高精）.cpp
+++ [算法1-1] 模拟与高精度/P1601-A+B Problem（高精）.cpp
@@ -1,37 +1,27 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> add(vector<int> &A, vector<int> &B) {
-	vector<int> res;
-	int t = 0;
-	for (int i = 0; i < A.size() || i < B.size(); i++) {
-		if (i < A.size())
-			t += A[i];
-		if (i < B.size())
-			t += B[i];
-		res.push_back(t % 10);
-		t /= 10;
-	}
-	if (t)
-		res.push_back(1);
-	return res;
+string add(string &A, string &B) {
+    string res = "";
+    int t = 0;
+    for (int i = 0; i < A.size() || i < B.size(); i++) {
+        if (i < A.size()) t += A[i] - '0';
+        if (i < B.size()) t += B[i] - '0';
+        res += (t % 10) + '0';
+        t /= 10;
+    }
+    if (t) res += '1';
+    return res;
 }
 
 int main() {
-	string s1, s2;
-	vector<int> a, b, sum;
-	cin >> s1 >> s2;
-	for (int i = s1.length() - 1; i >= 0; i--) {
-		a.push_back(s1[i] - '0');
-	}
-	for (int i = s2.length() - 1; i >= 0; i--) {
-		b.push_back(s2[i] - '0');
-	}
-	sum = add(a, b);
-	for (int i = sum.size() - 1; i >= 0; i--) {
-		cout << sum[i];
-	}
-	return 0;
+    string s1, s2, sum;
+    cin >> s1 >> s2;
+    reverse(s1.begin(), s1.end());
+    reverse(s2.begin(), s2.end());
+    sum = add(s1, s2);
+    for (int i = sum.size() - 1; i >= 0; i--) {
+        cout << sum[i];
+    }
+    return 0;
 }
