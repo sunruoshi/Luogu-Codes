b97e7ece48d5017a51fc5625083656c276e7e49a
sunruoshi <ruoshisun@gmail.com>
2021-07-16 11:14:20 +0800

    [refactor] refactor P1781 code

--- [算法1-2] 排序/P1781-宇宙总统.cpp
+++ [算法1-2] 排序/P1781-宇宙总统.cpp
@@ -1,27 +1,9 @@
-// P1781-宇宙总统
-// 思路
-// 这个题由于数据范围很大（100位数字），所以不能用数字类型来处理。
-// 考虑使用字符串类型读入得票数，自定义一个比较函数来比较数字字符
-// 串的大小。由于我们只需要知道最大值，所以不并需要排序，在读入字
-// 符串的同时维护最大值即可。
-// 这种使用其他数据类型来处理超大数的思想，就是高精度计算的思想。
-
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
 
 bool isGreater(string s1, string s2) {
-    // 如果两个数长度（位数）相同，则从高位到低位逐位比较
-    if (s1.length() == s2.length()) {
-        for (int i = 0; i < s1.length(); i++) {
-            if (s1[i] != s2[i]) {
-                return s1[i] - '0' > s2[i] - '0';
-            }
-        }
-    } else {
-        // 否则，位数多的数字一定大
-        return s1.length() > s2.length();
-    }
+    if (s1.size() == s2.size()) return s1 > s2;
+    else return s1.size() > s2.size();
 }
 
 int main() {
@@ -31,7 +13,6 @@ int main() {
     for (int i = 0; i < n; i++) {
         string s;
         cin >> s;
-        // 如果当前数字大于max，则更新max和id
         if (isGreater(s, max_t)) {
             max_t = s;
             id = i + 1;
