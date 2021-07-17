88b42df9edcf1ccff95632fe51e6f48fbf095616
sunruoshi <ruoshisun@gmail.com>
2021-07-17 19:00:25 +0800

    [refactor] refactor P1219 code

--- [算法1-7] 搜索/P1219-[USACO1.5]-八皇后 Checker Challenge.cpp
+++ [算法1-7] 搜索/P1219-[USACO1.5]-八皇后 Checker Challenge.cpp
@@ -8,18 +8,21 @@ void dfs(int row, int col, int x1, int x2) {
         cnt++;
         if (cnt <= 3) {
             for (int i = 0; i < n; i++) {
-                cout << queen[i] << " ";
+                printf("%d ", queen[i]);
             }
-            cout << endl;
+            printf("\n");
         }
         return;
     }
     int pos = ((1 << n) - 1) & (~(col | x1 | x2));
-    while (pos != 0) {
+    while (pos) {
         int p = pos & (-pos);
         pos = pos & (pos - 1);
-        // __builtin_ctz(x): 求x的二进制数末尾0的个数
-        int column = __builtin_ctz(p) + 1;
+        int column = 1, t = p;
+        while (!(t & 1)) {
+            column++;
+            t >>= 1;
+        }
         queen[row] = column;
         dfs(row + 1, col | p, (x1 | p) >> 1, (x2 | p) << 1);
         queen[row] = 0;
@@ -27,8 +30,8 @@ void dfs(int row, int col, int x1, int x2) {
 }
 
 int main() {
-    cin >> n;
+    scanf("%d", &n);
     dfs(0, 0, 0, 0);
-    cout << cnt;
+    printf("%d", cnt);
     return 0;
 }
