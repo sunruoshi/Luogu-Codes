a3ac84f83d5c925020fd636053584b7957b75eb6
Sam <sam@teamzoon.com>
2021-07-19 16:27:17 +0800

    [update] update P1562 code

--- [Others]/P1562-还是N皇后.cpp
+++ [Others]/P1562-还是N皇后.cpp
@@ -13,11 +13,7 @@ void dfs(int row, int col, int x1, int x2) {
     while (pos) {
         int p = pos & (-pos);
         pos = pos & (pos - 1);
-        int column = 0, t = p;
-        while (!(t & 1)) {
-            column++;
-            t >>= 1;
-        }
+        int column = __builtin_ctz(p);
         if (board[row][column] != '.') {
             dfs(row + 1, col | p, (x1 | p) >> 1, (x2 | p) << 1);
         }
