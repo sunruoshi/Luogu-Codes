e079674187fc63db667b5b867877a74939307496
Sam <sam@teamzoon.com>
2021-07-21 22:52:31 +0800

    [refactor] refactor P2392 code

--- [算法1-3] 暴力枚举/P2392-kkksc03考前临时抱佛脚.cpp
+++ [算法1-3] 暴力枚举/P2392-kkksc03考前临时抱佛脚.cpp
@@ -1,18 +1,18 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int ans, t = 1200, s[4];
+int ans, t, s[4], temp[20];
 
-void dfs(int cur, int left, int right, vector<int>& temp) {
-    if (cur == temp.size()) {
+void dfs(int cur, int left, int right, int len) {
+    if (cur == len) {
         t = min(t, max(left, right));
         return;
     }
     left += temp[cur];
-    dfs(cur + 1, left, right, temp);
+    dfs(cur + 1, left, right, len);
     left -= temp[cur];
     right += temp[cur];
-    dfs(cur + 1, left, right, temp);
+    dfs(cur + 1, left, right, len);
 }
 
 int main() {
@@ -20,12 +20,11 @@ int main() {
         cin >> s[i];
     }
     for (int i = 0; i < 4; i++) {
-        vector<int> temp (s[i]);
         t = 1200;
         for (int j = 0; j < s[i]; j++) {
             cin >> temp[j];
         }
-        dfs(0, 0, 0, temp);
+        dfs(0, 0, 0, s[i]);
         ans += t;
     }
     cout << ans;
