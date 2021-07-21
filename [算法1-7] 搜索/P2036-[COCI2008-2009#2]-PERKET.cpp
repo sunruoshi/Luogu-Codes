8e6e8648bea525c8da3fcde1539ec25fce5cf60c
Sam <sam@teamzoon.com>
2021-07-21 12:02:02 +0800

    [refactor] refactor P2036 code

--- [算法1-7] 搜索/P2036-[COCI2008-2009#2]-PERKET.cpp
+++ [算法1-7] 搜索/P2036-[COCI2008-2009#2]-PERKET.cpp
@@ -1,35 +1,30 @@
 #include <iostream>
-#include <vector>
 #include <cmath>
 using namespace std;
 
 int n, ans = 1e9, s[11], b[11];
-vector<int> temp;
-
-void dfs(int cur) {
-    if (cur > n) {
-        if (temp.size()) {
-            int ts = 1, tb = 0;
-            for (int i = 0; i < temp.size(); i++) {
-                ts *= s[temp[i]];
-                tb += b[temp[i]];
-            }
-            ans = min(ans, abs(ts - tb));
-        }
-        return;
-    }
-    temp.push_back(cur);
-    dfs(cur + 1);
-    temp.pop_back();
-    dfs(cur + 1);
-}
 
 int main() {
     cin >> n;
     for (int i = 1; i <= n; i++) {
         cin >> s[i] >> b[i];
     }
-    dfs(1);
+    for (int mask = 0; mask < (1 << n); mask++) {
+        int p = 0, temp[11];
+        for (int i = 0; i < n; i++) {
+            if (mask & (1 << i)) {
+                temp[p++] = i + 1;
+            }
+        }
+        if (p) {
+            int ts = 1, tb = 0;
+            for (int i = 0; i < p; i++) {
+                ts *= s[temp[i]];
+                tb += b[temp[i]];
+            }
+            ans = min(ans, abs(ts - tb));
+        }
+    }
     cout << ans;
     return 0;
 }
