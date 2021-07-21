a15c741eb8f4f5bb183da1237dfedb60e2d42d6d
Sam <sam@teamzoon.com>
2021-07-21 08:41:07 +0800

    [refactor] refactor P2404 code

--- [算法1-7] 搜索/P2404-自然数的拆分问题.cpp
+++ [算法1-7] 搜索/P2404-自然数的拆分问题.cpp
@@ -7,16 +7,16 @@ string s;
 
 void dfs(int start) {
     if (start > n || sum >= n) {
-        if (sum == n) cout << s.substr(0, s.length() - 1) << endl;
+        if (sum == n) cout << s.substr(0, s.size() - 1) << endl;
         return;
     }
     for (int i = 1; i < n; i++) {
-        if (s.length() == 0 || i >= s[s.length() - 2] - '0') {
+        if (s.size() == 0 || i >= s[s.size() - 2] - '0') {
             sum += i;
             s += i + '0';
             s += '+';
             dfs(start + 1);
-            s = s.substr(0, s.length() - 2);
+            s = s.substr(0, s.size() - 2);
             sum -= i;
         }
     }
@@ -24,7 +24,6 @@ void dfs(int start) {
 
 int main() {
     cin >> n;
-    string s;
     dfs(1);
     return 0;
 }
