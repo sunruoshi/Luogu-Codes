acd7a08ca598e9ddbb16a8485eb5f966eba29df4
Sam <sam@teamzoon.com>
2021-07-21 18:45:05 +0800

    [update] update P2036 code

--- [算法1-7] 搜索/P2036-[COCI2008-2009#2]-PERKET.cpp
+++ [算法1-7] 搜索/P2036-[COCI2008-2009#2]-PERKET.cpp
@@ -2,19 +2,19 @@
 #include <algorithm>
 using namespace std;
 
-int n, ans = 1e9, s[11], b[11];
+int n, ans = 1e9, s[10], b[10];
 
 int main() {
     scanf("%d", &n);
-    for (int i = 1; i <= n; i++) {
+    for (int i = 0; i < n; i++) {
         scanf("%d %d", &s[i], &b[i]);
     }
     for (int mask = 0; mask < (1 << n); mask++) {
         int ts = 1, tb = 0;
         for (int i = 0; i < n; i++) {
             if (mask & (1 << i)) {
-                ts *= s[i + 1];
-                tb += b[i + 1];
+                ts *= s[i];
+                tb += b[i];
                 ans = min(ans, abs(ts - tb));
             }
         }
