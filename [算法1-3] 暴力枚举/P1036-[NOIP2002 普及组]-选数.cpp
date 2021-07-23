7da0c0d6a36457008a6dc2707359ed1f2d701d4e
Sam <sam@teamzoon.com>
2021-07-23 10:59:58 +0800

    [refactor] refactor P1036 code

--- [算法1-3] 暴力枚举/P1036-[NOIP2002 普及组]-选数.cpp
+++ [算法1-3] 暴力枚举/P1036-[NOIP2002 普及组]-选数.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int n, k, arr[20];
+int n, k, ans, arr[20];
 
 bool isPrime(int d) {
     if (d < 2) return false;
@@ -12,20 +12,21 @@ bool isPrime(int d) {
     return true;
 }
 
-int dfs(int start, int sum, int cnt) {
-    if (cnt == k) return isPrime(sum);
-    int ans = 0;
-    for (int i = start; i < n; i++) {
-        ans += dfs(i + 1, sum + arr[i], cnt + 1);
-    }
-    return ans;
-}
-
 int main() {
     cin >> n >> k;
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    cout << dfs(0, 0, 0);
+    for (int mask = 0; mask < (1 << n); mask++) {
+        int sum = 0, cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (mask & (1 << i)) {
+                sum += arr[i];
+                cnt++;
+            }
+        }
+        if (cnt == k) ans += isPrime(sum);
+    }
+    cout << ans;
     return 0;
 }
