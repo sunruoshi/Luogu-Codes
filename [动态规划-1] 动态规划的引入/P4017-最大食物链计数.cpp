ba4968e992addee8c95ff1b4fd7209c1acfcc847
Sam <sam@teamzoon.com>
2021-07-25 16:56:14 +0800

    [update] update P4017 code

--- [动态规划-1] 动态规划的引入/P4017-最大食物链计数.cpp
+++ [动态规划-1] 动态规划的引入/P4017-最大食物链计数.cpp
@@ -1,4 +1,6 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <vector>
+#include <deque>
 using namespace std;
 
 const int MAXN = 5001, MOD = 80112002;
@@ -15,16 +17,16 @@ int main() {
         out[u]++;
         adj[u].push_back(v);
     }
-    queue<int> q;
+    deque<int> q;
     for (int i = 1; i <= n; i++) {
         if (!in[i]) {
             dp[i] = 1;
-            q.push(i);
+            q.push_back(i);
         }
     }
-    while (!q.empty()) {
+    while (q.size()) {
         int u = q.front();
-        q.pop();
+        q.pop_front();
         for (int j = 0; j < adj[u].size(); j++) {
             int v = adj[u][j];
             dp[v] += dp[u];
@@ -35,7 +37,7 @@ int main() {
                     ans += dp[v];
                     ans %= MOD;
                 }
-                q.push(v);
+                q.push_back(v);
             }
         }
     }
