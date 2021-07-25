39a065e149c19e69cc8672dc775842025e6b3370
Sam <sam@teamzoon.com>
2021-07-25 11:45:28 +0800

    [update] update P5076 code

--- [数据结构1-2] 二叉树/P5076-[深基16.例7]-普通二叉树(简化版).cpp
+++ [数据结构1-2] 二叉树/P5076-[深基16.例7]-普通二叉树(简化版).cpp
@@ -1,16 +1,24 @@
-#include <bits/stdc++.h>
+#include <cstdio>
 using namespace std;
 
 const int MAXN = 1e4, INF = 2147483647;
 
 struct Node {
     int val, left, right, weight, num;
+    Node() {}
+    Node(int _val, int _left, int _right, int _weight, int _num) {
+        val = _val;
+        left = _left;
+        right = _right;
+        weight = _weight;
+        num = _num;
+    }
 } node[MAXN];
 
 int idx = 1, root;
 
 int newNode(int v) {
-    node[idx] = Node({v, 0, 0, 1, 1});
+    node[idx] = Node(v, 0, 0, 1, 1);
     return idx++;
 }
 
@@ -31,12 +39,9 @@ int getRank(int x, int root) {
 }
 
 int getNum(int x, int root) {
-    if (x <= node[node[root].left].weight) 
-        return getNum(x, node[root].left);
-    else if (x <= node[node[root].left].weight + node[root].num) 
-        return node[root].val;
-    else 
-        return getNum(x - node[node[root].left].weight - node[root].num, node[root].right);
+    if (x <= node[node[root].left].weight) return getNum(x, node[root].left);
+    else if (x <= node[node[root].left].weight + node[root].num) return node[root].val;
+    else return getNum(x - node[node[root].left].weight - node[root].num, node[root].right);
 }
 
 int findPrev(int x, int root, int res) {
@@ -75,15 +80,15 @@ void insertNode(int &root, int x) {
 int main() {
     int q;
     scanf("%d", &q);
-    node[0] = Node({0, 0, 0, 0, 0});
+    node[0] = Node(0, 0, 0, 0, 0);
     while (q--) {
-        int op, x;
-        scanf("%d %d", &op, &x);
-        if (op == 1) printf("%d\n", getRank(x, root));
-        else if (op == 2) printf("%d\n", getNum(x, root));
-        else if (op == 3) printf("%d\n", findPrev(x, root, -INF));
-        else if (op == 4) printf("%d\n", findNext(x, root, INF));
-        else if (op == 5) insertNode(root, x);
+        int opt, x;
+        scanf("%d %d", &opt, &x);
+        if (opt == 1) printf("%d\n", getRank(x, root));
+        else if (opt == 2) printf("%d\n", getNum(x, root));
+        else if (opt == 3) printf("%d\n", findPrev(x, root, -INF));
+        else if (opt == 4) printf("%d\n", findNext(x, root, INF));
+        else if (opt == 5) insertNode(root, x);
     }
     return 0;
 }
