7d311c5e655fd6cc0c129ea6288fbe1cb7447ac1
Sam <sam@teamzoon.com>
2021-07-26 19:32:41 +0800

    [update] update P1196 code

--- [NOI]/P1196-[NOI2002]-银河英雄传说.cpp
+++ [NOI]/P1196-[NOI2002]-银河英雄传说.cpp
@@ -1,23 +1,24 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
 using namespace std;
 
 const int MAXN = 30001;
 
-int n, fa[MAXN], dist[MAXN], num[MAXN];
+int n, fa[MAXN], dis[MAXN], size[MAXN];
 
 int Find(int x) {
     if (x == fa[x]) return x;
     int fx = Find(fa[x]);
-    dist[x] += dist[fa[x]];
+    dis[x] += dis[fa[x]];
     fa[x] = fx;
     return fx;
 }
 
 void Union(int x, int y) {
     int fx = Find(x), fy = Find(y);
-    dist[fx] += num[fy];
-    num[fy] += num[fx];
-    num[fx] = 0;
+    dis[fx] += size[fy];
+    size[fy] += size[fx];
+    size[fx] = 0;
     fa[fx] = fy;
 }
 
@@ -25,17 +26,17 @@ int main() {
     cin >> n;
     for (int i = 1; i <= MAXN; i++) {
         fa[i] = i;
-        num[i] = 1;
+        size[i] = 1;
     }
     while (n--) {
-        char op;
+        char opt;
         int i, j;
-        cin >> op >> i >> j;
-        if (op == 'M') Union(i, j);
+        cin >> opt >> i >> j;
+        if (opt == 'M') Union(i, j);
         else {
             int fi = Find(i), fj = Find(j);
             if (fi != fj) cout << "-1" << endl;
-            else cout << abs(dist[i] - dist[j]) - 1 << endl;
+            else cout << abs(dis[i] - dis[j]) - 1 << endl;
         }
     }
     return 0;
