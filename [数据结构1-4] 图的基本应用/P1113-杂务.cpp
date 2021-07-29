6b92b400e6990ad50979e67522ddde2764ef0b4b
Sam <sam@teamzoon.com>
2021-07-30 00:52:31 +0800

    [update] update P1113 code

--- [数据结构1-4] 图的基本应用/P1113-杂务.cpp
+++ [数据结构1-4] 图的基本应用/P1113-杂务.cpp
@@ -4,21 +4,21 @@ using namespace std;
 
 const int MAXN = 10001;
 
-int n, ans, w[MAXN];
+int n, ans, dp[MAXN];
 
 int main() {
     scanf("%d", &n);
     for (int i = 1; i <= n; i++) {
         int u, len, temp = 0;
         scanf("%d %d", &u, &len);
-        while (true) {
+        while (1) {
             int pre;
             scanf("%d", &pre);
             if (!pre) break;
-            temp = max(temp, w[pre]);
+            temp = max(temp, dp[pre]);
         }
-        w[u] = temp + len;
-        ans = max(ans, w[u]);
+        dp[u] = temp + len;
+        ans = max(ans, dp[u]);
     }
     printf("%d", ans);
     return 0;
