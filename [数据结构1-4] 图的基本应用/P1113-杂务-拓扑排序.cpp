f49ab33b2cf15cc41e05dcd707c9108433e90358
Sam <sam@teamzoon.com>
2021-07-30 11:49:54 +0800

    [update] update P1113 code

--- [数据结构1-4] 图的基本应用/P1113-杂务-拓扑排序.cpp
+++ [数据结构1-4] 图的基本应用/P1113-杂务-拓扑排序.cpp
@@ -6,27 +6,27 @@ using namespace std;
 
 const int MAXN = 1e5 + 1;
 
-int n, ans, in[MAXN], dp[MAXN], len[MAXN];
+int n, ans, in[MAXN], len[MAXN], f[MAXN];
 vector<int> adj[MAXN];
 
 int main() {
     scanf("%d", &n);
     for (int i = 1; i <= n; i++) {
-        int u;
-        scanf("%d %d", &u, &len[i]);
+        int v;
+        scanf("%d %d", &v, &len[i]);
         while (1) {
-            int pre;
-            scanf("%d", &pre);
-            if (!pre) break;
-            adj[pre].push_back(u);
-            in[u]++;
+            int u;
+            scanf("%d", &u);
+            if (!u) break;
+            adj[u].push_back(v);
+            in[v]++;
         }
     }
     deque<int> q;
     for (int i = 1; i <= n; i++) {
         if (!in[i]) {
             q.push_back(i);
-            dp[i] = len[i];
+            f[i] = len[i];
         }
     }
     while (q.size()) {
@@ -34,11 +34,11 @@ int main() {
         q.pop_front();
         for (int i = 0; i < adj[u].size(); i++) {
             int v = adj[u][i];
-            dp[v] = max(dp[v], dp[u] + len[v]);
+            f[v] = max(f[v], f[u] + len[v]);
             in[v]--;
             if (!in[v]) q.push_back(v);
         }
-        ans = max(ans, dp[u]);
+        ans = max(ans, f[u]);
     }
     printf("%d", ans);
     return 0;
