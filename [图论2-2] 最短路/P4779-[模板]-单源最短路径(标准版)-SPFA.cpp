21c47839361a4e857e3b61e19eadf89922666c66
Sam <sam@teamzoon.com>
2021-07-31 13:06:41 +0800

    [update] update P4779 SPFA code

--- [图论2-2] 最短路/P4779-[模板]-单源最短路径(标准版)-SPFA.cpp
+++ [图论2-2] 最短路/P4779-[模板]-单源最短路径(标准版)-SPFA.cpp
@@ -1,11 +1,29 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <vector>
+#include <queue>
 using namespace std;
 
 const int MAXN = 100001, INF = 0x3f3f3f3f;
 
 struct Edge {
     int v, w;
+    Edge(int _v, int _w) {
+        v = _v;
+        w = _w;
+    }
 };
+
+struct Node {
+    int v, dis;
+    Node(int _v, int _dis) {
+        v = _v;
+        dis = _dis;
+    }
+};
+
+bool operator < (Node a, Node b) {
+    return a.dis > b.dis;
+}
 
 vector<Edge> adj[MAXN];
 int n, m, s, dis[MAXN], num[MAXN];
@@ -13,21 +31,21 @@ bool inq[MAXN];
 
 bool SPFA(int s) {
     fill(dis, dis + MAXN, INF);
-    priority_queue<int> q;
-    q.push(s);
+    priority_queue<Node> q;
+    q.push(Node(s, 0));
     inq[s] = 1;
     num[s]++;
     dis[s] = 0;
     while (!q.empty()) {
-        int u = q.top();
+        Node u = q.top();
         q.pop();
-        inq[u] = 0;
-        for (int j = 0; j < adj[u].size(); j++) {
-            int v = adj[u][j].v;
-            if (dis[u] + adj[u][j].w < dis[v]) {
-                dis[v] = dis[u] + adj[u][j].w;
+        inq[u.v] = 0;
+        for (unsigned int i = 0; i < adj[u.v].size(); i++) {
+            int v = adj[u.v][i].v;
+            if (dis[u.v] + adj[u.v][i].w < dis[v]) {
+                dis[v] = dis[u.v] + adj[u.v][i].w;
                 if (!inq[v]) {
-                    q.push(v);
+                    q.push(Node(v, dis[v]));
                     inq[v] = 1;
                     num[v]++;
                     if (num[v] >= n) return false;
@@ -43,7 +61,7 @@ int main() {
     for (int i = 1; i <= m; i++) {
         int u, v, w;
         scanf("%d %d %d", &u, &v, &w);
-        adj[u].push_back(Edge({v, w}));
+        adj[u].push_back(Edge(v, w));
     }
     SPFA(s);
     for (int i = 1; i <= n; i++) {
