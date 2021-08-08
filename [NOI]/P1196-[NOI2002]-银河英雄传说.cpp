011748114c989b9a167caf5049df9a224aa9abe2
Sam <sam@teamzoon.com>
2021-08-08 13:21:49 +0800

    [refactor] refactor P1196 code

--- [NOI]/P1196-[NOI2002]-银河英雄传说.cpp
+++ [NOI]/P1196-[NOI2002]-银河英雄传说.cpp
@@ -4,41 +4,45 @@ using namespace std;
 
 const int MAXN = 30001;
 
-int n, fa[MAXN], dis[MAXN], size[MAXN];
+int n, dis[MAXN];
 
-int Find(int x) {
-    if (x != fa[x]) {
-        int temp = fa[x];
-        fa[x] = Find(fa[x]);
-        dis[x] += dis[temp];
+struct UnionFind {
+    int fa[MAXN], size[MAXN];
+    UnionFind() {
+        for (int i = 1; i <= MAXN; i++) {
+            fa[i] = i;
+            size[i] = 1;
+        }
     }
-    return fa[x];
-}
-
-void Union(int x, int y) {
-    int xx = Find(x), yy = Find(y);
-    dis[xx] += size[yy];
-    size[yy] += size[xx];
-    size[xx] = 0;
-    fa[xx] = yy;
-}
+    int Find(int x) {
+        if (x != fa[x]) {
+            int temp = fa[x];
+            fa[x] = Find(fa[x]);
+            dis[x] += dis[temp];
+        }
+        return fa[x];
+    }
+    void Union(int x, int y) {
+        int xx = Find(x), yy = Find(y);
+        dis[xx] += size[yy];
+        size[yy] += size[xx];
+        size[xx] = 0;
+        fa[xx] = yy;
+    }
+};
 
 int main() {
     cin >> n;
-    for (int i = 1; i <= MAXN; i++) {
-        fa[i] = i;
-        size[i] = 1;
-    }
+    UnionFind uf;
     while (n--) {
         char opt;
         int i, j;
         cin >> opt >> i >> j;
-        if (opt == 'M') Union(i, j);
+        if (opt == 'M') uf.Union(i, j);
         else {
-            if (Find(i) != Find(j)) cout << "-1" << endl;
+            if (uf.Find(i) != uf.Find(j)) cout << "-1" << endl;
             else cout << abs(dis[i] - dis[j]) - 1 << endl;
         }
     }
     return 0;
 }
-
