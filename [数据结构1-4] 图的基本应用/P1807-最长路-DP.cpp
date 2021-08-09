9a5feba4820e71c8fc1277df0759cdc3b02df465
Sam <sam@teamzoon.com>
2021-08-09 15:56:07 +0800

    [refactor] refactor P1807 DP code

--- [数据结构1-4] 图的基本应用/P1807-最长路-DP.cpp
+++ [数据结构1-4] 图的基本应用/P1807-最长路-DP.cpp
@@ -7,10 +7,7 @@ const int MAXN = 1501, INF = 0x3f3f3f3f;
 
 struct Edge {
     int v, w;
-    Edge(int _v, int _w) {
-        v = _v;
-        w = _w;
-    }
+    Edge(int _v, int _w) : v(_v), w(_w) {}
 };
 
 int n, m, dis[MAXN];
@@ -18,18 +15,17 @@ vector<Edge> adj[MAXN];
 
 int dfs(int u) {
     if (dis[u] > -INF) return dis[u];
-    for (unsigned int i = 0; i < adj[u].size(); i++) {
-        int v = adj[u][i].v;
-        dis[u] = max(dis[u], dfs(v) + adj[u][i].w);
+    for (Edge edge : adj[u]) {
+        int v = edge.v, w = edge.w;
+        dis[u] = max(dis[u], dfs(v) + w);
     }
     return dis[u];
 }
 
 int main() {
     scanf("%d %d", &n, &m);
-    for (int i = 2; i <= n; i++) {
-        dis[i] = -INF;
-    }
+    fill(dis, dis + MAXN, -INF);
+    dis[1] = 0;
     for (int i = 0; i < m; i++) {
         int u, v, w;
         scanf("%d %d %d", &u, &v, &w);
