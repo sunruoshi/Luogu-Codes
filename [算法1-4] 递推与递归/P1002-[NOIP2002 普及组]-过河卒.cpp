4c3bbb272ed8857a5934dd2b0921f2dfa84edd73
Sam <sam@teamzoon.com>
2021-08-10 17:58:52 +0800

    [refactor] refactor P1002 code

--- [算法1-4] 递推与递归/P1002-[NOIP2002 普及组]-过河卒.cpp
+++ [算法1-4] 递推与递归/P1002-[NOIP2002 普及组]-过河卒.cpp
@@ -1,23 +1,23 @@
-#include <iostream>
-#include <cmath>
+#include <cstdio>
+#include <algorithm>
 using namespace std;
 
-int ban[23][23];
-long long dp[23][23];
+int ban[22][22];
+long long dp[22][22];
 
 int main() {
     int n, m, hx, hy, dirs[9][2] = {{0, 0}, {2, 1}, {1, 2}, {-1, 2}, {-2, 1}, {-2, -1}, {-1, -2}, {1, -2}, {2, -1}};
-    cin >> n >> m >> hx >> hy;
+    scanf("%d %d %d %d", &n, &m, &hx, &hy);
     for (int i = 0; i < 9; i++) {
-        int dx = hx + 2 + dirs[i][0], dy = hy + 2 + dirs[i][1];
-        ban[dx][dy]++;
+        int dx = hx + 1 + dirs[i][0], dy = hy + 1 + dirs[i][1];
+        if (dx >= 1 && dx <= n + 1 && dy >= 1 && dy <= m + 1) ban[dx][dy]++;
     }
-    dp[2][2] = 1;
-    for (int i = 2; i <= n + 2; i++) {
-        for (int j = 2; j <= m + 2; j++) {
+    dp[1][1] = 1;
+    for (int i = 1; i <= n + 1; i++) {
+        for (int j = 1; j <= m + 1; j++) {
             if (!ban[i][j]) dp[i][j] = max(dp[i][j], dp[i - 1][j] + dp[i][j - 1]);
         }
     }
-    cout << dp[n + 2][m + 2];
+    printf("%lld", dp[n + 1][m + 1]);
     return 0;
 }
