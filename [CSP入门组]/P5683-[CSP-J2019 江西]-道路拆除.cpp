2bd1f74a3f16a0695842e9a502fbfa5b064bce33
Sam <sam@teamzoon.com>
2021-08-10 21:52:56 +0800

    [refactor] refactor P5683 code

--- [CSP入门组]/P5683-[CSP-J2019 江西]-道路拆除.cpp
+++ [CSP入门组]/P5683-[CSP-J2019 江西]-道路拆除.cpp
@@ -1,36 +1,39 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <queue>
+#include <algorithm>
 using namespace std;
 
 const int MAXN = 3001, INF = 0x3f3f3f3f;
 
 struct Node {
     int v, dis;
+    Node(int _v, int _dis) : v(_v), dis(_dis) {}
+    bool operator < (const Node &a) const {
+        return dis > a.dis;
+    }
 };
 
-bool operator < (Node a, Node b) {
-    return a.dis > b.dis;
-}
-
+int n, m, s1, t1, s2, t2, ans = INF, dis[MAXN], dis1[MAXN], dis2[MAXN];
 vector<int> adj[MAXN];
-int n, m, dis[MAXN], dis1[MAXN], dis2[MAXN];
 
-void dijkstra(int s) {
+void dijkstra(int s, int dis[]) {
     bool visited[MAXN];
     memset(visited, 0, sizeof(visited));
     fill(dis, dis + MAXN, INF);
-    priority_queue<Node> q;
     dis[s] = 0;
-    q.push(Node({s, 0}));
-    while (!q.empty()) {
+    priority_queue<Node> q;
+    q.push(Node(s, 0));
+    while (q.size()) {
         int u = q.top().v;
         q.pop();
         if (visited[u]) continue;
         visited[u] = 1;
-        for (int j = 0; j < adj[u].size(); j++) {
-            int v = adj[u][j];
+        for (int v : adj[u]) {
             if (!visited[v] && dis[u] + 1 < dis[v]) {
                 dis[v] = dis[u] + 1;
-                q.push(Node({v, dis[v]}));
+                q.push(Node(v, dis[v]));
             }
         }
     }
@@ -44,14 +47,10 @@ int main() {
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-    int s1, t1, s2, t2;
     scanf("%d %d %d %d", &s1, &t1, &s2, &t2);
-    dijkstra(s1);
-    copy(dis, dis + n + 1, dis1);
-    dijkstra(s2);
-    copy(dis, dis + n + 1, dis2);
-    dijkstra(1);
-    int ans = INF;
+    dijkstra(s1, dis1);
+    dijkstra(s2, dis2);
+    dijkstra(1, dis);
     for (int i = 1; i <= n; i++) {
         if (dis[i] + dis1[i] <= t1 && dis[i] + dis2[i] <= t2) {
             ans = min(ans, dis[i] + dis1[i] + dis2[i]);
