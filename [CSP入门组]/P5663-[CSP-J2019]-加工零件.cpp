97aa7033e5f88f751b43371e6eb01a9714f486f6
Sam <sam@teamzoon.com>
2021-08-10 22:03:21 +0800

    [refactor] refactor P5663 code

--- [CSP入门组]/P5663-[CSP-J2019]-加工零件.cpp
+++ [CSP入门组]/P5663-[CSP-J2019]-加工零件.cpp
@@ -1,58 +1,56 @@
-#include <bits/stdc++.h>
+#include <cstdio>
+#include <vector>
+#include <queue>
+#include <algorithm>
 using namespace std;
 
 const int MAXN = 100001, INF = 0x3f3f3f3f;
 
 struct Node {
     int v, dis;
+    Node(int _v, int _dis) : v(_v), dis(_dis) {}
+    bool operator < (const Node &a) const {
+        return dis > a.dis;
+    }
 };
 
-bool operator < (Node a, Node b) {
-    return a.dis > b.dis;
-}
-
+int n, m, t, dis[MAXN][2];
 vector<int> adj[MAXN];
-int n, m, q, dis[MAXN][2];
 bool visited[MAXN][2];
 
-void dijkstra(int s) {
-    fill(dis[0], dis[0] + MAXN * 2, INF);
-    priority_queue<Node> q;
-    dis[s][0] = 0;
-    q.push(Node({s, 0}));
-    while (!q.empty()) {
-        int u = q.top().v;
-        q.pop();
-        for (int j = 0; j < adj[u].size(); j++) {
-            int v = adj[u][j];
-            if (dis[u][1] + 1 < dis[v][0]) {
-                dis[v][0] = dis[u][1] + 1;
-                if (!visited[v][0]) {
-                    visited[v][0] = 1;
-                    q.push(Node({v, dis[v][0]}));
-                }
-            }
-            if (dis[u][0] + 1 < dis[v][1]) {
-                dis[v][1] = dis[u][0] + 1;
-                if (!visited[v][1]) {
-                    visited[v][1] = 1;
-                    q.push(Node({v, dis[v][1]}));
-                }
-            }
-        }
-    }
-}
-
 int main() {
-    scanf("%d %d %d", &n, &m, &q);
+    scanf("%d %d %d", &n, &m, &t);
     for (int i = 1; i <= m; i++) {
         int u, v;
         scanf("%d %d", &u, &v);
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-    dijkstra(1);
-    while (q--) {
+    fill(dis[0], dis[0] + MAXN * 2, INF);
+    priority_queue<Node> q;
+    dis[1][0] = 0;
+    q.push(Node(1, 0));
+    while (q.size()) {
+        int u = q.top().v;
+        q.pop();
+        for (int v : adj[u]) {
+            if (dis[u][1] + 1 < dis[v][0]) {
+                dis[v][0] = dis[u][1] + 1;
+                if (!visited[v][0]) {
+                    visited[v][0] = 1;
+                    q.push(Node(v, dis[v][0]));
+                }
+            }
+            if (dis[u][0] + 1 < dis[v][1]) {
+                dis[v][1] = dis[u][0] + 1;
+                if (!visited[v][1]) {
+                    visited[v][1] = 1;
+                    q.push(Node(v, dis[v][1]));
+                }
+            }
+        }
+    }
+    while (t--) {
         int a, L;
         scanf("%d %d", &a, &L);
         if (dis[a][L % 2] <= L) printf("Yes\n");
