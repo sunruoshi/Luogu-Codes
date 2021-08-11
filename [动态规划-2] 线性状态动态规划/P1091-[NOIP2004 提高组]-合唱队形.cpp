569f8324b442e0f9945bc5495e5fce801e27a9bd
Sam <sam@teamzoon.com>
2021-08-11 23:19:28 +0800

    [refactor] refactor P1091 code

--- [动态规划-2] 线性状态动态规划/P1091-[NOIP2004 提高组]-合唱队形.cpp
+++ [动态规划-2] 线性状态动态规划/P1091-[NOIP2004 提高组]-合唱队形.cpp
@@ -4,24 +4,24 @@ using namespace std;
 
 const int MAXN = 101;
 
-int n, ans, arr[MAXN], dp1[MAXN], dp2[MAXN];
+int n, ans, p1, p2, arr[MAXN], dp1[MAXN], dp2[MAXN], f1[MAXN], f2[MAXN];
 
 int main() {
     scanf("%d", &n);
     for (int i = 1; i <= n; i++) {
         scanf("%d", &arr[i]);
     }
-    for (int i = 1; i <= n; i++) {
-        dp1[i] = 1;
-        for (int j = 1; j < i; j++) {
-            if (arr[i] > arr[j]) dp1[i] = max(dp1[i], dp1[j] + 1);
-        }
+    f1[++p1] = arr[1], f2[++p2] = arr[n];
+    dp1[1] = dp2[n] = 1;
+    for (int i = 2; i <= n; i++) {
+        if (arr[i] > f1[p1]) f1[++p1] = arr[i];
+        else f1[lower_bound(f1 + 1, f1 + p1 + 1, arr[i]) - f1] = arr[i];
+        dp1[i] = p1;
     }
-    for (int i = n; i >= 1; i--) {
-        dp2[i] = 1;
-        for (int j = n; j > i; j--) {
-            if (arr[i] > arr[j]) dp2[i] = max(dp2[i], dp2[j] + 1);
-        }
+    for (int i = n - 1; i >= 1; i--) {
+        if (arr[i] > f2[p2]) f2[++p2] = arr[i];
+        else f2[lower_bound(f2 + 1, f2 + p2 + 1, arr[i]) - f2] = arr[i];
+        dp2[i] = p2;
     }
     for (int i = 1; i <= n; i++) {
         ans = max(ans, dp1[i] + dp2[i] - 1);
