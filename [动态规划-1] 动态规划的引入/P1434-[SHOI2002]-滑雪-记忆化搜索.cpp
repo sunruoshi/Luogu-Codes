b4e1a6e65b32d290ec918a925f43bf5bc91f9663
Sam <sam@teamzoon.com>
2021-08-11 18:07:13 +0800

    [update] update P1434 code

--- [动态规划-1] 动态规划的引入/P1434-[SHOI2002]-滑雪-记忆化搜索.cpp
+++ [动态规划-1] 动态规划的引入/P1434-[SHOI2002]-滑雪-记忆化搜索.cpp
@@ -2,18 +2,18 @@
 #include <algorithm>
 using namespace std;
 
-int r, c, ans, m[100][100], temp[100][100], dirs[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+int r, c, ans, m[100][100], dp[100][100], dirs[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
-int dfs(int x, int y) {
-    if (temp[x][y]) return temp[x][y];
-    temp[x][y] = 1;
+int DP(int x, int y) {
+    if (dp[x][y]) return dp[x][y];
+    dp[x][y] = 1;
     for (int i = 0; i < 4; i++) {
         int nx = x + dirs[i][0], ny = y + dirs[i][1];
         if (nx >= 0 && nx < r && ny >= 0 && ny < c && m[nx][ny] < m[x][y]) {
-            temp[x][y] = max(temp[x][y], dfs(nx, ny) + 1);
+            dp[x][y] = max(dp[x][y], DP(nx, ny) + 1);
         }
     }
-    return temp[x][y];
+    return dp[x][y];
 }
 
 int main() {
@@ -25,7 +25,7 @@ int main() {
     }
     for (int i = 0; i < r; i++) {
         for (int j = 0; j < c; j++) {
-            ans = max(ans, dfs(i, j));
+            ans = max(ans, DP(i, j));
         }
     }
     printf("%d", ans);
