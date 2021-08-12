e9f0499429fda360f308ddcc8963be644d385f66
Sam <sam@teamzoon.com>
2021-08-12 21:46:18 +0800

    [update] update P1020 code

--- [动态规划-2] 线性状态动态规划/P1020-[NOIP1999 普及组]-导弹拦截.cpp
+++ [动态规划-2] 线性状态动态规划/P1020-[NOIP1999 普及组]-导弹拦截.cpp
@@ -5,16 +5,16 @@ using namespace std;
 
 const int MAXN = 1e5 + 1;
 
-int n, p1, p2, arr[MAXN], dp1[MAXN], dp2[MAXN];
+int n, p1, p2, arr[MAXN], f1[MAXN], f2[MAXN];
 
 int main() {
     while (~scanf("%d", &arr[++n]));
-    dp1[++p1] = dp2[++p2] = arr[1];
+    f1[++p1] = f2[++p2] = arr[1];
     for (int i = 2; i < n; i++) {
-        if (arr[i] <= dp1[p1]) dp1[++p1] = arr[i];
-        else dp1[upper_bound(dp1 + 1, dp1 + p1 + 1, arr[i], greater<int>()) - dp1] = arr[i];
-        if (arr[i] > dp2[p2]) dp2[++p2] = arr[i];
-        else dp2[lower_bound(dp2 + 1, dp2 + p2 + 1, arr[i]) - dp2] = arr[i];
+        if (arr[i] <= f1[p1]) f1[++p1] = arr[i];
+        else f1[upper_bound(f1 + 1, f1 + p1 + 1, arr[i], greater<int>()) - f1] = arr[i];
+        if (arr[i] > f2[p2]) f2[++p2] = arr[i];
+        else f2[lower_bound(f2 + 1, f2 + p2 + 1, arr[i]) - f2] = arr[i];
     }
     printf("%d\n%d", p1, p2);
     return 0;
