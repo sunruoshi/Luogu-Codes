794ea471cfaabc5d9848275cb953d7c92a833d14
Sam <sam@teamzoon.com>
2021-08-14 13:44:47 +0800

    [refactor] refactor P3366 code

--- [图论2-3] 最小生成树/P3366-[模板]-最小生成树.cpp
+++ [图论2-3] 最小生成树/P3366-[模板]-最小生成树.cpp
@@ -1,18 +1,19 @@
 #include <cstdio>
-#include <algorithm>
+#include <queue>
 using namespace std;
 
-const int MAXV = 5001, MAXE = 200001;
+const int MAXN = 5001;
 
 struct Edge {
     int u, v, w;
+    Edge(int _u, int _v, int _w) : u(_u), v(_v), w(_w) {}
     bool operator < (const Edge &a) const {
-        return w < a.w;
+        return w > a.w;
     }
-} edge[MAXE];
+};
 
 struct UnionFind {
-    int fa[MAXV];
+    int fa[MAXN];
     UnionFind(int n) {
         for (int i = 1; i <= n; i++) {
             fa[i] = i;
@@ -27,31 +28,28 @@ struct UnionFind {
     }
 };
 
-int kruskal(int n, int m) {
-    int ans = 0, num = 0;
-    UnionFind uf(n);
-    sort(edge, edge + m);
-    for (int i = 1; i <= m; i++) {
-        int u = edge[i].u, v = edge[i].v;
-        if (uf.Find(u) != uf.Find(v)) {
-            uf.Union(u, v);
-            ans += edge[i].w;
-            num++;
-            if (num == n - 1) break;
-        }
-    }
-    if (num != n - 1) return -1;
-    else return ans;
-}
+int n, m, num, ans;
 
 int main() {
     int n, m;
     scanf("%d %d", &n, &m);
+    UnionFind uf(n);
+    priority_queue<Edge> q;
     for (int i = 1; i <= m; i++) {
-        scanf("%d %d %d", &edge[i].u, &edge[i].v, &edge[i].w);
+        int u, v, w;
+        scanf("%d %d %d", &u, &v, &w);
+        q.push(Edge(u, v, w));
     }
-    int ans = kruskal(n, m);
-    if (ans == -1) printf("orz");
-    else printf ("%d", ans);
+    while (num < n - 1 && q.size()) {
+        Edge edge = q.top();
+        q.pop();
+        if (uf.Find(edge.u) != uf.Find(edge.v)) {
+            uf.Union(edge.u, edge.v);
+            ans += edge.w;
+            num++;
+        }
+    }
+    if (num != n - 1) printf("orz");
+    else printf("%d", ans);
     return 0;
 }
