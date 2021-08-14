60c2fbb6325a13c55206f1b50e3c15f107e67866
Sam <sam@teamzoon.com>
2021-08-14 22:26:36 +0800

    [add] add P1195 code

---
+++ [图论2-3] 最小生成树/P1195-口袋的天空.cpp
@@ -0,0 +1,54 @@
+#include <cstdio>
+#include <queue>
+using namespace std;
+
+const int MAXN = 1001;
+
+struct Edge {
+    int u, v, w;
+    Edge(int _u, int _v, int _w) : u(_u), v(_v), w(_w) {}
+    bool operator < (const Edge &a) const {
+        return w > a.w;
+    }
+};
+
+struct UnionFind {
+    int fa[MAXN];
+    UnionFind(int n) {
+        for (int i = 1; i <= n; i++) {
+            fa[i] = i;
+        }
+    }
+    int Find(int x) {
+        if (x != fa[x]) fa[x] = Find(fa[x]);
+        return fa[x];
+    }
+    void Union(int x, int y) {
+        fa[Find(x)] = Find(y);
+    }
+};
+
+int n, m, k, num, ans;
+
+int main() {
+    scanf("%d %d %d", &n, &m, &k);
+    UnionFind uf(n);
+    priority_queue<Edge> q;
+    for (int i = 1; i <= m; i++) {
+        int u, v, w;
+        scanf("%d %d %d", &u, &v, &w);
+        q.push(Edge(u, v, w));
+    }
+    while (num < n - k && q.size()) {
+        Edge edge = q.top();
+        q.pop();
+        if (uf.Find(edge.u) != uf.Find(edge.v)) {
+            uf.Union(edge.u, edge.v);
+            ans += edge.w;
+            num++;
+        }
+    }
+    if (num == n - k) printf("%d", ans);
+    else printf("No Answer");
+    return 0;
+}
