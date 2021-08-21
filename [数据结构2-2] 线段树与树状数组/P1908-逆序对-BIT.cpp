b1c445c67e9746b3da4d58792365570862b51264
Sam <sam@teamzoon.com>
2021-08-21 19:45:04 +0800

    [fix] fix varible names of P1908

--- [数据结构2-2] 线段树与树状数组/P1908-逆序对-BIT.cpp
+++ [数据结构2-2] 线段树与树状数组/P1908-逆序对-BIT.cpp
@@ -8,9 +8,9 @@ const int MAXN = 500001;
 
 struct Node {
     int val, pos;
-} temp[MAXN];
+} arr[MAXN];
 
-int a[MAXN], c[MAXN];
+int Hash[MAXN], c[MAXN];
 
 void update(int x, int v) {
     for (int i = x; i < MAXN; i += lowbit(i)) {
@@ -30,17 +30,17 @@ int main() {
     int n;
     scanf("%d", &n);
     for (int i = 1; i <= n; i++) {
-        scanf("%d", &temp[i].val);
-        temp[i].pos = i;
+        scanf("%d", &arr[i].val);
+        arr[i].pos = i;
     }
-    sort(temp + 1, temp + n + 1, [](Node a, Node b) { return a.val == b.val ? a.pos < b.pos : a.val < b.val; });
+    sort(arr + 1, arr + n + 1, [](Node a, Node b) { return a.val == b.val ? a.pos < b.pos : a.val < b.val; });
     for (int i = 1; i <= n; i++) {
-        a[temp[i].pos] = i;
+        Hash[arr[i].pos] = i;
     }
     long long ans = 0;
     for (int i = 1; i <= n; i++) {
-        update(a[i], 1);
-        ans += i - getSum(a[i]);
+        update(Hash[i], 1);
+        ans += i - getSum(Hash[i]);
     }
     printf("%lld", ans);
     return 0;
