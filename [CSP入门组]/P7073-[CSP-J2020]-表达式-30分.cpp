f5e589791f0b3ce4659d4b15cd1042722e01088c
Sam <sam@teamzoon.com>
2021-08-26 12:15:08 +0800

    [update] update P7073 30points code

--- [CSP入门组]/P7073-[CSP-J2020]-表达式-30分.cpp
+++ [CSP入门组]/P7073-[CSP-J2020]-表达式-30分.cpp
@@ -14,36 +14,36 @@ int main() {
     for (int i = 1; i <= n; i++) {
         cin >> num[i];
     }
-    string rpn;
-    for (int i = 0; i < s.length(); i++) {
+    string expr;
+    for (unsigned int i = 0; i < s.length(); i++) {
         if (s[i] == 'x') {
             int j = i + 1, temp = 0;
             while (s[j] != ' ') {
                 temp *= 10;
                 temp += s[j++] - '0';
             }
-            rpn += num[temp];
+            expr += num[temp];
             id[temp] = cnt++;
-        } else if (s[i] == '&' || s[i] == '|' || s[i] == '!') rpn += s[i];
+        } else if (s[i] == '&' || s[i] == '|' || s[i] == '!') expr += s[i];
     }
-    rpn += '@';
+    expr += '@';
     int q;
     cin >> q;
     for (int i = 0; i < q; i++) {
         int idx, p = 0, cnt = 1;
         cin >> idx;
         vector<int> stk;
-        while (rpn[p] != '@') {
-            if (rpn[p] == '0' || rpn[p] == '1') {
-                int num = rpn[p] - '0';
+        while (expr[p] != '@') {
+            if (expr[p] == '0' || expr[p] == '1') {
+                int num = expr[p] - '0';
                 if (cnt++ == id[idx]) num = !num;
                 stk.push_back(num);
-            } else if (rpn[p] == '&' || rpn[p] == '|') {
+            } else if (expr[p] == '&' || expr[p] == '|') {
                 int num1 = stk.back();
                 stk.pop_back();
                 int num2 = stk.back();
                 stk.pop_back();
-                if (rpn[p] == '&') stk.push_back(num1 & num2);
+                if (expr[p] == '&') stk.push_back(num1 & num2);
                 else stk.push_back(num1 | num2);
             } else {
                 stk.back() = !stk.back();
