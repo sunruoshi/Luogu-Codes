b5bfe8bd5fcc18c0b85d788aa874691e34b39054
Sam <sam@teamzoon.com>
2021-08-28 14:55:42 +0800

    [refactor] refactor P7073 code from static building to dunamic building

--- [CSP入门组]/P7073-[CSP-J2020]-表达式.cpp
+++ [CSP入门组]/P7073-[CSP-J2020]-表达式.cpp
@@ -5,33 +5,28 @@
 #define MAXN 500001
 using namespace std;
 
-int idx, n, q, value[MAXN];
+int n, q, value[MAXN];
 bool check[MAXN];
 string s;
 
 struct Node {
-    int val, opt, left, right;
-    Node() {}
-    Node(int _v, int _o, int _l, int _r) : val(_v), opt(_o), left(_l), right(_r) {}
-} node[MAXN];
+    int val, opt;
+    Node *left, *right;
+    Node(int _v, int _o, Node* _l, Node* _r) : val(_v), opt(_o), left(_l), right(_r) {}
+};
 
-int newNode(int val, int opt, int left, int right) {
-    node[idx] = Node(val, opt, left, right);
-    return idx++;
-}
-
-void dfs(int root) {
-    if (node[root].opt > 0) {
-        check[node[root].opt] = 1;
+void dfs(Node* root) {
+    if (root->opt > 0) {
+        check[root->opt] = 1;
         return;
     }
-    int left = node[root].left, right = node[root].right;
-    if (node[root].opt == -1) {
-        if (node[left].val == 1) dfs(right);
-        if (node[right].val == 1) dfs(left);
-    } else if (node[root].opt == -2) {
-        if (node[left].val == 0) dfs(right);
-        if (node[right].val == 0) dfs(left);
+    Node *left = root->left, *right = root->right;
+    if (root->opt == -1) {
+        if (left->val == 1) dfs(right);
+        if (right->val == 1) dfs(left);
+    } else if (root->opt == -2) {
+        if (left->val == 0) dfs(right);
+        if (right->val == 0) dfs(left);
     } else dfs(left);
 }
 
@@ -43,7 +38,7 @@ int main() {
     for(int i = 1; i <= n; i++) {
         cin >> value[i];
     }
-    deque<int> stk;
+    deque<Node*> stk;
     for(unsigned int i = 0; i < s.size(); i++) {
         if (s[i] == 'x') {
             i++;
@@ -53,35 +48,35 @@ int main() {
                 temp += s[i] - '0';
                 i++;
             }
-            stk.push_back(newNode(value[temp], temp, -1, -1));
+            stk.push_back(new Node(value[temp], temp, NULL, NULL));
         } else if (s[i] == '&') {
-            int right = stk.back();
+            Node* right = stk.back();
             stk.pop_back();
-            int left = stk.back();
+            Node* left = stk.back();
             stk.pop_back();
-            int val = node[left].val & node[right].val;
-            stk.push_back(newNode(val, -1, left, right));
+            int val = left->val & right->val;
+            stk.push_back(new Node(val, -1, left, right));
         } else if (s[i] == '|') {
-            int right = stk.back();
+            Node* right = stk.back();
             stk.pop_back();
-            int left = stk.back();
+            Node* left = stk.back();
             stk.pop_back();
-            int val = node[left].val | node[right].val;
-            stk.push_back(newNode(val, -2, left, right));
+            int val = left->val | right->val;
+            stk.push_back(new Node(val, -2, left, right));
         } else if (s[i] == '!') {
-            int left = stk.back();
+            Node* left = stk.back();
             stk.pop_back();
-            int val = !node[left].val;
-            stk.push_back(newNode(val, -3, left, -1));
+            int val = !left->val;
+            stk.push_back(new Node(val, -3, left, NULL));
         }
     }
-    int root = stk.back();
+    Node* root = stk.back();
     dfs(root);
     scanf("%d", &q);
     while (q--) {
         int x;
         scanf("%d", &x);
-        printf("%d\n", check[x] ^ node[root].val);
+        printf("%d\n", check[x] ^ root->val);
     }
     // fclose(stdin);
     // fclose(stdout);
