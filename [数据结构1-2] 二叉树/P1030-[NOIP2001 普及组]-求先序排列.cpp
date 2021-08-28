1a7a591ff88a0a717905c0a74bbfe37caecd1928
Sam <sam@teamzoon.com>
2021-08-28 15:10:04 +0800

    [refactor] refactor P1030 P1827 code

--- [数据结构1-2] 二叉树/P1030-[NOIP2001 普及组]-求先序排列.cpp
+++ [数据结构1-2] 二叉树/P1030-[NOIP2001 普及组]-求先序排列.cpp
@@ -7,41 +7,34 @@ char inOrder[MAXN], postOrder[MAXN];
 
 struct Node {
     char val;
-    int left, right;
-    Node() {}
-    Node(char _val, int _left, int _right) : val(_val), left(_left), right(_right) {}
-} node[MAXN];
+    Node *left, *right;
+    Node(char _val, Node* _left, Node* _right) : val(_val), left(_left), right(_right) {}
+};
 
-int idx;
-
-int newNode(char v) {
-    node[idx] = Node(v, -1, -1);
-    return idx++;
-}
-
-int create(int inL, int inR, int postL, int postR) {
-    if (postL > postR) return -1;
-    int root = newNode(postOrder[postR]), k;
+Node* create(int inL, int inR, int postL, int postR) {
+    if (postL > postR) return NULL;
+    Node* root = new Node(postOrder[postR], NULL, NULL);
+    int k;
     for (k = inL; k <= inR; k++) {
         if (inOrder[k] == postOrder[postR]) break;
     }
     int numL = k - inL;
-    node[root].left = create(inL, k - 1, postL, postL + numL - 1);
-    node[root].right = create(k + 1, inR, postL + numL, postR - 1);
+    root->left = create(inL, k - 1, postL, postL + numL - 1);
+    root->right = create(k + 1, inR, postL + numL, postR - 1);
     return root;
 }
 
-void preOrder(int root) {
-    if (root == -1) return;
-    printf("%c", node[root].val);
-    preOrder(node[root].left);
-    preOrder(node[root].right);
+void preOrder(Node* root) {
+    if (root == NULL) return;
+    printf("%c", root->val);
+    preOrder(root->left);
+    preOrder(root->right);
 }
 
 int main() {
     scanf("%s %s", inOrder, postOrder);
     int n = strlen(inOrder);
-    int root = create(0, n - 1, 0, n - 1);
+    Node* root = create(0, n - 1, 0, n - 1);
     preOrder(root);
     return 0;
 }
--- [数据结构1-2] 二叉树/P1827-[USACO3.4]-美国血统 American Heritage.cpp
+++ [数据结构1-2] 二叉树/P1827-[USACO3.4]-美国血统 American Heritage.cpp
@@ -7,41 +7,34 @@ char inOrder[MAXN], preOrder[MAXN];
 
 struct Node {
     char val;
-    int left, right;
-    Node() {}
-    Node(char _val, int _left, int _right) : val(_val) , left(_left), right(_right) {}
-} node[MAXN];
+    Node *left, *right;
+    Node(char _val, Node* _left, Node* _right) : val(_val) , left(_left), right(_right) {}
+};
 
-int idx;
-
-int newNode(char v) {
-    node[idx] = Node(v, -1, -1);
-    return idx++;
-}
-
-int create(int inL, int inR, int preL, int preR) {
-    if (preL > preR) return -1;
-    int root = newNode(preOrder[preL]), k;
+Node* create(int inL, int inR, int preL, int preR) {
+    if (preL > preR) return NULL;
+    Node* root = new Node(preOrder[preL], NULL, NULL);
+    int k;
     for (k = inL; k <= inR; k++) {
         if (inOrder[k] == preOrder[preL]) break;
     }
     int numL = k - inL;
-    node[root].left = create(inL, k - 1, preL + 1, preL + numL);
-    node[root].right = create(k + 1, inR, preL + numL + 1, preR);
+    root->left = create(inL, k - 1, preL + 1, preL + numL);
+    root->right = create(k + 1, inR, preL + numL + 1, preR);
     return root;
 }
 
-void postOrder(int root) {
-    if (root == -1) return;
-    postOrder(node[root].left);
-    postOrder(node[root].right);
-    printf("%c", node[root].val);
+void postOrder(Node* root) {
+    if (root == NULL) return;
+    postOrder(root->left);
+    postOrder(root->right);
+    printf("%c", root->val);
 }
 
 int main() {
     scanf("%s %s", inOrder, preOrder);
     int n = strlen(inOrder);
-    int root = create(0, n - 1, 0, n - 1);
+    Node* root = create(0, n - 1, 0, n - 1);
     postOrder(root);
     return 0;
 }
