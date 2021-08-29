f03be553cc48430de5e15d1943ef34ba565d9151
Sam <sam@teamzoon.com>
2021-08-29 20:49:55 +0800

    [add] add P3379 code

---
+++ [图论2-1] 基础树上问题/P3379-[模板]-最近公共祖先(LCA).cpp
@@ -0,0 +1,54 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#define MAXN 500001
+using namespace std;
+
+int fa[MAXN][31], depth[MAXN];
+vector<int> node[MAXN];
+
+void dfs(int root, int parent) {
+    fa[root][0] = parent;
+    depth[root] = depth[parent] + 1;
+    for (int i = 1; (1 << i) <= depth[root]; i++) {
+        fa[root][i] = fa[fa[root][i - 1]][i - 1];
+    }
+    for (int v : node[root]) {
+        if (v != parent) dfs(v, root);
+    }
+}
+
+int lca(int x, int y) {
+    if (depth[x] < depth[y]) swap(x, y);
+    int temp = depth[x] - depth[y];
+    for (int i = 0; temp > 0; i++) {
+        if (temp & 1) x = fa[x][i];
+        temp >>= 1;
+    }
+    if (x == y) return x;
+    for (int i = 30; i >= 0; i--) {
+        if (fa[x][i] != fa[y][i]) {
+            x = fa[x][i];
+            y = fa[y][i];
+        }
+    }
+    return fa[x][0];
+}
+
+int main() {
+    int n, m, root;
+    scanf("%d %d %d", &n, &m, &root);
+    for (int i = 1; i < n; i++) {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        node[x].push_back(y);
+        node[y].push_back(x);
+    }
+    dfs(root, 0);
+    for (int i = 1; i <= m; i++) {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        printf("%d\n", lca(x, y));
+    }
+    return 0;
+}
