6ce74cdcfabe0822aacccda5921e983206425c8e
Sam <sam@teamzoon.com>
2021-08-31 11:59:45 +0800

    [refactor] refactor P1364 code

--- [数据结构1-2] 二叉树/P1364-医院设置.cpp
+++ [数据结构1-2] 二叉树/P1364-医院设置.cpp
@@ -1,47 +1,52 @@
 #include <cstdio>
+#include <vector>
 #include <algorithm>
+#define INF 0x3f3f3f3f
 using namespace std;
-
-const int MAXN = 101, INF = 0x3f3f3f;
 
 struct Node {
     int val, left, right;
-} node[MAXN];
+};
 
-int n, ans = INF, dis[MAXN], size[MAXN];
+int n, ans = INF;
+vector<int> dis, size;
+vector<Node> node;
 
-void preTreat(int root, int depth) {
+void dfs(int root, int depth) {
     size[root] = node[root].val;
     if (node[root].left) {
-        preTreat(node[root].left, depth + 1);
+        dfs(node[root].left, depth + 1);
         size[root] += size[node[root].left];
     }
     if (node[root].right) {
-        preTreat(node[root].right, depth + 1);
+        dfs(node[root].right, depth + 1);
         size[root] += size[node[root].right];
     }
     dis[1] += node[root].val * depth;
 }
 
-void findCog(int root) {
+void cog(int root) {
     if (node[root].left) {
         dis[node[root].left] = dis[root] + size[1] - 2 * size[node[root].left];
-        findCog(node[root].left); 
+        cog(node[root].left); 
     }
     if (node[root].right) {
         dis[node[root].right] = dis[root] + size[1] - 2 * size[node[root].right];
-        findCog(node[root].right);
+        cog(node[root].right);
     }
     ans = min(ans, dis[root]);
 }
 
 int main() {
     scanf("%d", &n);
+    node.resize(n + 1);
+    dis.resize(n + 1);
+    size.resize(n + 1);
     for (int i = 1; i <= n; i++) {
         scanf("%d %d %d", &node[i].val, &node[i].left, &node[i].right);
     }
-    preTreat(1, 0);
-    findCog(1);
+    dfs(1, 0);
+    cog(1);
     printf("%d", ans);
     return 0;
 }
