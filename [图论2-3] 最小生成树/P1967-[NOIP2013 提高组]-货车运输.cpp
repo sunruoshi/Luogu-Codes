cd4b3ca24716e2cdfddfeddec2fd845e166caee7
Sam <sam@teamzoon.com>
2021-09-02 18:59:22 +0800

    [refactoe] refactor P1967 code

--- [图论2-3] 最小生成树/P1967-[NOIP2013 提高组]-货车运输.cpp
+++ [图论2-3] 最小生成树/P1967-[NOIP2013 提高组]-货车运输.cpp
@@ -1,8 +1,6 @@
 #include <cstdio>
 #include <vector>
 #include <algorithm>
-#define MAXN 10000
-#define INF 0x3f3f3f3f
 using namespace std;
 
 struct Edge {
@@ -19,72 +17,85 @@ struct MSTEdge {
     MSTEdge(int _v, int _w) : v(_v), w(_w) {}
 };
 
-struct UnionFind {
-    vector<int> fa;
-    UnionFind(int n) {
-        fa.resize(n + 1);
-        for (int i = 1; i <= n; i++) {
-            fa[i] = i;
+class UnionFind {
+    private:
+        vector<int> fa;
+
+    public:
+        UnionFind(int n) {
+            fa.resize(n + 1);
+            for (int i = 1; i <= n; i++) {
+                fa[i] = i;
+            }
         }
-    }
-    int Find(int x) {
-        if (x != fa[x]) fa[x] = Find(fa[x]);
-        return fa[x];
-    }
-    bool Union(int x, int y) {
-        int xx = Find(x), yy = Find(y);
-        if (xx == yy) return false;
-        fa[xx] = yy;
-        return true;
-    }
+        int Find(int x) {
+            if (x != fa[x]) fa[x] = Find(fa[x]);
+            return fa[x];
+        }
+        bool Union(int x, int y) {
+            int xx = Find(x), yy = Find(y);
+            if (xx == yy) return false;
+            fa[xx] = yy;
+            return true;
+        }
 };
 
-int n, m, q, num, p[MAXN][31], w[MAXN][31], depth[MAXN];
-vector<Edge> edge;
-vector<vector<MSTEdge>> mst;
-
-void dfs(int u, int fa, int weight) {
-    p[u][0] = fa;
-    w[u][0] = weight;
-    depth[u] = depth[fa] + 1;
-    for (int i = 1; (1 << i) <= depth[u]; i++) {
-        p[u][i] = p[p[u][i - 1]][i - 1];
-        w[u][i] = min(w[u][i - 1], w[p[u][i - 1]][i - 1]);
-    }
-    for (auto edge : mst[u]) {
-        int v = edge.v, w = edge.w;
-        if (v != fa) dfs(v, u, w);
-    }
-}
-
-int lca(int x, int y, UnionFind &uf) {
-    if (uf.Find(x) != uf.Find(y)) return -1;
-    int res = INF;
-    if (depth[x] < depth[y]) swap(x, y);
-    int temp = depth[x] - depth[y];
-    for (int i = 0; temp > 0; i++) {
-        if (temp & 1) {
-            res = min(res, w[x][i]);
-            x = p[x][i];
+class LCA {
+    private:
+        vector<vector<int>> p, w;
+        vector<int> depth;
+        void init(int u, int fa, int weight, vector<vector<MSTEdge>> &mst) {
+            p[u][0] = fa;
+            w[u][0] = weight;
+            depth[u] = depth[fa] + 1;
+            for (int i = 1; (1 << i) <= depth[u]; i++) {
+                p[u][i] = p[p[u][i - 1]][i - 1];
+                w[u][i] = min(w[u][i - 1], w[p[u][i - 1]][i - 1]);
+            }
+            for (auto edge : mst[u]) {
+                int v = edge.v, w = edge.w;
+                if (v != fa) init(v, u, w, mst);
+            }
         }
-        temp >>= 1;
-    }
-    if (x == y) return res;
-    for (int i = 30; i >= 0; i--) {
-        if (p[x][i] != p[y][i]) {
-            res = min(res, min(w[x][i], w[y][i]));
-            x = p[x][i];
-            y = p[y][i];
+
+    public:
+        LCA(int n, int m, vector<vector<MSTEdge>> &mst) {
+            p.resize(n, vector<int>(m));
+            w.resize(n, vector<int>(m));
+            depth.resize(n);
+            init(1, 0, 0x3f3f3f3f, mst);
         }
-    }
-    res = min(res, min(w[x][0], w[y][0]));
-    return res;
-}
+        int solve(int x, int y, UnionFind &uf) {
+            if (uf.Find(x) != uf.Find(y)) return -1;
+            int res = 0x3f3f3f3f;
+            if (depth[x] < depth[y]) swap(x, y);
+            int temp = depth[x] - depth[y];
+            for (int i = 0; temp > 0; i++) {
+                if (temp & 1) {
+                    res = min(res, w[x][i]);
+                    x = p[x][i];
+                }
+                temp >>= 1;
+            }
+            if (x == y) return res;
+            for (int i = 30; i >= 0; i--) {
+                if (p[x][i] != p[y][i]) {
+                    res = min(res, min(w[x][i], w[y][i]));
+                    x = p[x][i];
+                    y = p[y][i];
+                }
+            }
+            res = min(res, min(w[x][0], w[y][0]));
+            return res;
+        }
+};
 
 int main() {
+    int n, m, q, num = 0;
     scanf("%d %d", &n, &m);
     UnionFind uf(n);
-    mst.resize(n + 1);
+    vector<vector<MSTEdge>> mst(n + 1);
+    vector<Edge> edge;
     for (int i = 1; i <= m; i++) {
         int u, v, w;
         scanf("%d %d %d", &u, &v, &w);
@@ -99,12 +110,12 @@ int main() {
             num++;
         }
     }
-    dfs(1, 0, INF);
+    LCA lca(n + 1, 31, mst);
     scanf("%d", &q);
     while (q--) {
         int x, y;
         scanf("%d %d", &x, &y);
-        printf("%d\n", lca(x, y, uf));
+        printf("%d\n", lca.solve(x, y, uf));
     }
     return 0;
 }
