c6e906b1a16e4e6f993e42a81897540a27a03ea2
Sam <sam@teamzoon.com>
2021-09-13 19:39:22 +0800

    [refactor] refactor findLoop function

--- [图论2-4] 连通性问题/P5022-[NOIP2018 提高组]-旅行.cpp
+++ [图论2-4] 连通性问题/P5022-[NOIP2018 提高组]-旅行.cpp
@@ -5,31 +5,24 @@
 #define MAXN 5001
 using namespace std;
 
-int n, m, tag, top, latest;
-int dfn[MAXN], low[MAXN], stk[MAXN];
-bool inStack[MAXN], circle[MAXN], visited[MAXN], flag;
+int n, m, tag, latest;
+int dfn[MAXN], fa[MAXN];
+bool loop[MAXN], visited[MAXN], flag;
 vector<int> adj[MAXN];
 
-void tarjan(int u, int fa) {
-    dfn[u] = low[u] = ++tag;
-    stk[++top] = u;
-    int pos = top;
-    inStack[u] = 1;
+void findLoop(int u) {
+    dfn[u] = ++tag;
     for (int v : adj[u]) {
-        if (v == fa) continue;
-        if (!dfn[v]) {
-            tarjan(v, u);
-            low[u] = min(low[u], low[v]);
-        } else if (inStack[v]) {
-            low[u] = min(low[u], dfn[v]);
-        }
-    }
-    if (dfn[u] == low[u]) {
-        if (pos == top) inStack[stk[top--]] = 0;
-        while (pos <= top) {
-            circle[stk[top]] = 1;
-            inStack[stk[top]] = 0;
-            top--;
+        if (v == fa[u]) continue;
+        if (dfn[v]) {
+            if (dfn[v] < dfn[u]) continue;
+            loop[v] = 1;
+            while (v != u) {
+                loop[v = fa[v]] = 1;
+            }
+        } else {
+            fa[v] = u;
+            findLoop(v);
         }
     }
 }
@@ -43,7 +36,7 @@ void dfs(int u) {
         if (!visited[v]) q.push(v);
     }
     while (q.size()) {
-        if (!circle[u] || !circle[q.top()]) {
+        if (!loop[u] || !loop[q.top()]) {
             dfs(q.top());
             q.pop();
         } else {
@@ -73,7 +66,7 @@ int main() {
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-    tarjan(1, 0);
+    findLoop(1);
     dfs(1);
     return 0;
 }
