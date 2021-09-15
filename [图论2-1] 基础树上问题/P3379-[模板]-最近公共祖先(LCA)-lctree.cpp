c870b79c8b6d51b2882c67d6cc0281256e6a01e7
Sam <sam@teamzoon.com>
2021-09-15 10:55:28 +0800

    [add] add P3379 link cut tree code

---
+++ [图论2-1] 基础树上问题/P3379-[模板]-最近公共祖先(LCA)-lctree.cpp
@@ -0,0 +1,55 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+#define MAXN 500001
+using namespace std;
+
+int n, m, root;
+int fa[MAXN], depth[MAXN], size[MAXN], son[MAXN], top[MAXN];
+vector<int> adj[MAXN];
+
+void dfs1(int u) {
+    size[u] = 1;
+    depth[u] = depth[fa[u]] + 1;
+    for (int v : adj[u]) {
+        if (v == fa[u]) continue;
+        fa[v] = u;
+        dfs1(v);
+        size[u] += size[v];
+        if (!son[u] || size[v] > size[son[u]]) son[u] = v;
+    }
+}
+
+void dfs2(int u, int tp) {
+    top[u] = tp;
+    if (son[u]) dfs2(son[u], tp);
+    for (int v : adj[u]) {
+        if (v != son[u] && v != fa[u]) dfs2(v, v);
+    }
+}
+
+int lca(int u, int v) {
+    while (top[u] != top[v]) {
+        if (depth[top[u]] > depth[top[v]]) u = fa[top[u]];
+        else v = fa[top[v]];
+    }
+    return depth[u] < depth[v] ? u : v;
+}
+
+int main() {
+    scanf("%d %d %d", &n, &m, &root);
+    for (int i = 1; i < n; i++) {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        adj[x].push_back(y);
+        adj[y].push_back(x);
+    }
+    dfs1(root);
+    dfs2(root, root);
+    while (m--) {
+        int x, y;
+        scanf("%d %d", &x, &y);
+        printf("%d\n", lca(x, y));
+    }
+    return 0;
+}
