67b1938b823ae636e9442551354fe73d7c44d316
Sam <sam@teamzoon.com>
2021-09-20 22:37:20 +0800

    [refactor] refactor P3370 code

--- [数据结构1-3] 集合/P3370-[模板]-字符串哈希.cpp
+++ [数据结构1-3] 集合/P3370-[模板]-字符串哈希.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 #include <string>
-#include <unordered_set>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+const int MOD = 1e9 + 7, P = 1e7 + 19;
+
+long long HashStr(string s) {
+    long long id = 0;
+    for (unsigned i = 0; i < s.size(); i++) {
+        id = (id * P + s[i] - 'a') % MOD;
+    }
+    return id;
+}
+
 int main() {
-    int n;
+    int n, ans = 1;
     cin >> n;
-    unordered_set<string> hashSet;
+    vector<int> HashSet;
     while (n--) {
         string s;
         cin >> s;
-        hashSet.insert(s);
+        HashSet.push_back(HashStr(s));
     }
-    cout << hashSet.size();
+    sort(HashSet.begin(), HashSet.end());
+    for (unsigned i = 1; i < HashSet.size(); i++) {
+        if (HashSet[i] != HashSet[i - 1]) ans++;
+    }
+    cout << ans;
     return 0;
 }
