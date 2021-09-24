bdbba542736e623c6c8000d8847cdfba73b91cef
Sam <sam@teamzoon.com>
2021-09-24 15:45:20 +0800

    [refactor] refactor P1996 code

--- [数据结构1-1] 线性表/P1996-约瑟夫问题.cpp
+++ [数据结构1-1] 线性表/P1996-约瑟夫问题.cpp
@@ -1,29 +1,33 @@
-// P1996-约瑟夫问题 模拟法
-// 思路
-// 使用数组下标作为编号，值用来标记是否出队
-// 使用一个变量cnt来标记当前报到的数字，当cnt==m时，当前位置的人需要出队
-// 使用一个变量total来计数，当所有人都出队时退出循环
-#include <iostream>
-using namespace std;
+#include <cstdio>
+
+struct Node {
+    int val;
+    Node* next;
+};
 
 int main() {
-    int n, m, p = 0, total = 0, cnt = 0; // p指针标记当前报数的人
-    cin >> n >> m;
-    int a[n + 1];
-    for (int i = 1; i <= n; i++) {
-        a[i] = 0; // 数组初始化为0，0代表还在队中
+    int n, m;
+    scanf("%d %d", &n, &m);
+    Node *head = new Node, *cur;
+    head->val = 1;
+    head->next = NULL;
+    cur = head;
+    for (int i = 2; i <= n; i++) {
+        Node* p = new Node;
+        p->val = i;
+        p->next = NULL;
+        cur->next = p;
+        cur = p;
     }
-    while (total < n) {
-        if (a[p % (n + 1)] == 0) {
-            cnt++;
-            if (cnt == m) {
-                cout << p % (n + 1) << ' ';
-                a[p % (n + 1)] = 1;
-                cnt = 0;
-                total++;
-            }
+    cur->next = head;
+    cur = head;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j < m - 1; j++) {
+            cur = cur->next;
         }
-        p++;
+        printf("%d ", cur->next->val);
+        cur->next = cur->next->next;
+        cur = cur->next;
     }
     return 0;
 }
