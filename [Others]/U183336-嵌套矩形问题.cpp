69aabdb06a50f09ac2ff07b8688f1d2252022382
Sam <sam@teamzoon.com>
2021-10-09 22:46:52 +0800

    [update] update U183336 code

--- [Others]/U183336-嵌套矩形问题.cpp
+++ [Others]/U183336-嵌套矩形问题.cpp
@@ -1,20 +1,17 @@
 #include <cstdio>
+#include <cstring>
 #include <vector>
 #include <algorithm>
-#define MAXN 1001
 using namespace std;
 
 struct Rect {
     int a, b;
-} rect[MAXN];
+};
 
-int n, ans, start, dp[MAXN], Next[MAXN];
-vector<int> adj[MAXN];
-
-int DP(int u) {
+int DP(int u, int* dp, int* Next, vector<int>* adj) {
     if (dp[u]) return dp[u];
     for (int v : adj[u]) {
-        int temp = DP(v);
+        int temp = DP(v, dp, Next, adj);
         if (temp > dp[u]) {
             dp[u] = temp;
             Next[u] = v;
@@ -25,26 +22,38 @@ int DP(int u) {
 }
 
 int main() {
-    auto check = [&](Rect x, Rect y) {
-        return (x.a < y.a && x.b < y.b) || (x.b < y.a && x.a < y.b);
-    };
+    int n;
     scanf("%d", &n);
+
+    Rect rect[n + 1];
     for (int i = 1; i <= n; i++) {
         scanf("%d %d", &rect[i].a, &rect[i].b);
     }
+
+    vector<int> adj[n + 1];
+    auto check = [&](Rect x, Rect y) {
+        return (x.a < y.a && x.b < y.b) || (x.b < y.a && x.a < y.b);
+    };
+
     for (int u = 1; u < n; u++) {
         for (int v = u + 1; v <= n; v++) {
             if (check(rect[u], rect[v])) adj[u].push_back(v);
             if (check(rect[v], rect[u])) adj[v].push_back(u);
         }
     }
+
+    int dp[n + 1], Next[n + 1], ans = 0, start = 0;
+    memset(dp, 0, sizeof(dp));
+    memset(Next, 0, sizeof(Next));
+
     for (int i = 1; i <= n; i++) {
-        dp[i] = DP(i);
+        dp[i] = DP(i, dp, Next, adj);
         if (dp[i] > ans) {
             ans = dp[i];
             start = i;
         }
     }
+
     printf("%d\n", ans);
     while (start) {
         printf("%d ", start);
