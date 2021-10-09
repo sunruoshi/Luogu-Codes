5b8efef42aaa8c4b2ae645016f8b800ed6831570
Sam <sam@teamzoon.com>
2021-10-09 16:19:00 +0800

    [add] add U183372 code

---
+++ [Others]/U183372-硬币问题.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>
+#include <algorithm>
+#define INF 0x3f3f3f3f
+using namespace std;
+
+int main() {
+    int n, s;
+    scanf("%d %d", &n, &s);
+    int minf[n + 1], maxf[n + 1], v[n + 1];
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &v[i]);
+    }
+    fill(minf + 1, minf + n + 1, INF);
+    fill(maxf + 1, maxf + n + 1, -INF);
+    minf[0] = maxf[0] = 0;
+    for (int i = 1; i <= s; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i >= v[j]) {
+                minf[i] = min(minf[i], minf[i - v[j]] + 1);
+                maxf[i] = max(maxf[i], maxf[i - v[j]] + 1);
+            }
+        }
+    }
+    printf("%d\n%d", minf[s], maxf[s]);
+    return 0;
+}
