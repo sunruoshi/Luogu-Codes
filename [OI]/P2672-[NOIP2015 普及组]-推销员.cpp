92e49a28b0667323f1ac7ac3be07933a75edf27d
Sam <sam@teamzoon.com>
2021-10-12 16:35:01 +0800

    [fix] fix P2669 and P2672

--- [OI]/P2672-[NOIP2015 普及组]-推销员.cpp
+++ [OI]/P2672-[NOIP2015 普及组]-推销员.cpp
@@ -1,21 +1,19 @@
 #include <cstdio>
-#include <cstring>
 #include <algorithm>
+#define MAXN 100001
 using namespace std;
 
 struct Node {
     int dis, val;
 };
 
+int sum[MAXN], leftMax[MAXN], rightMax[MAXN];
+
 int main() {
     // freopen("salesman.in", "r", stdin);
     // freopen("salesman.out", "w", stdout);
     int n;
     scanf("%d", &n);
-    int sum[n + 1], leftMax[n + 1], rightMax[n + 1];
-    memset(sum, 0, sizeof(sum));
-    memset(leftMax, 0, sizeof(leftMax));
-    memset(rightMax, 0, sizeof(rightMax));
     Node node[n + 1];
     for (int i = 1; i <= n; i++) {
         scanf("%d", &node[i].dis);
