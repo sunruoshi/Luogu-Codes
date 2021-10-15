1c225e3f2948ec06985bc3fe268db3f4ce23b26c
Sam <sam@teamzoon.com>
2021-10-15 13:10:56 +0800

    [add] add CF 1600E code

---
+++ [Others]/CodeForces-1600E-Array Game.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int a[n + 1];
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", a + i);
+    }
+    int l = 1, r = n;
+    while (l < n && a[l] < a[l + 1]) l++;
+    while (r > 1 && a[r] < a[r - 1]) r--;
+    r = n - r - 1;
+    if ((l & 1) || (r & 1)) printf("Alice");
+    else printf("Bob");
+    return 0;
+}
