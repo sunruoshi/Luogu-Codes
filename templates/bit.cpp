d6558f546049c4eda1a83e84f4c233d08b5bb1d2
Sam <sam@teamzoon.com>
2021-11-14 12:18:07 +0800

    [add] add binary indexed tree template

---
+++ templates/bit.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+using namespace std;
+
+template <class T>
+class BinaryIndexedTree {
+    private:
+        vector<T> c;
+
+    public:
+        BinaryIndexedTree(size_t n) { c.resize(n); }
+        void update(size_t pos, T v);
+        T query(size_t pos);
+};
+
+template <class T>
+void BinaryIndexedTree<T>::update(size_t pos, T v) {
+    while (pos < c.size()) {
+        c[pos] += v;
+        pos += pos & (-pos);
+    }
+}
+
+template <class T>
+T BinaryIndexedTree<T>::query(size_t pos) {
+    T res = 0;
+    while (pos > 0) {
+        res += c[pos];
+        pos -= pos & (-pos);
+    }
+    return res;
+}
