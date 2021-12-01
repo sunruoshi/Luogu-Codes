2e623ecf00d02985c90fc2c04503384c994ebc39
Sam <sam@teamzoon.com>
2021-12-01 20:56:15 +0800

    [add] add OI style splay code

---
+++ templates/splay-oi.cpp
@@ -0,0 +1,99 @@
+#include <cstdlib>
+
+struct SplayNode {
+    int val;
+    SplayNode *L, *R, *F;
+    SplayNode(int x) : val(x), L(NULL), R(NULL), F(NULL) {}
+};
+
+void rightRotate(SplayNode* &cur) {
+    SplayNode* y = cur->F;
+    cur->F = y->F;
+    if (cur->F) {
+        if (y == cur->F->L) cur->F->L = cur;
+        else cur->F->R = cur;
+    }
+    if (cur->R) cur->R->F = y;
+    y->L = cur->R;
+    y->F = cur;
+    cur->R = y;
+}
+
+void leftRotate(SplayNode* &cur) {
+    SplayNode* y = cur->F;
+    cur->F = y->F;
+    if (cur->F) {
+        if (y == cur->F->L) cur->F->L = cur;
+        else cur->F->R = cur;
+    }
+    if (cur->L) cur->L->F = y;
+    y->R = cur->L;
+    y->F = cur;
+    cur->L = y;
+}
+
+void splay(SplayNode* x, SplayNode* &root) {
+    while (x->F) {
+        SplayNode* y = x->F;
+        SplayNode* z = y->F;
+        if (!z) {
+            if (x == y->L) rightRotate(x);
+            else leftRotate(x);
+        } else {
+            if (x == y->L && y == z->L) {
+                rightRotate(y);
+                rightRotate(x);
+            } else if (x == y->L && y == z->R) {
+                rightRotate(x);
+                leftRotate(x);
+            } else if (x == y->R && y == z->R) {
+                leftRotate(y);
+                leftRotate(x);
+            } else {
+                leftRotate(x);
+                rightRotate(x);
+            }
+        }
+    }
+    root = x;
+}
+
+SplayNode* insert(SplayNode* &cur, int x) {
+    if (x <= cur->val) {
+        if (!cur->L) {
+            cur->L = new SplayNode(x);
+            cur->L->F = cur;
+            return cur->L;
+        } else return insert(cur->L, x);
+    } else {
+        if (!cur->R) {
+            cur->R = new SplayNode(x);
+            cur->R->F = cur;
+            return cur->R;
+        } else return insert(cur->R, x);
+    }
+}
+
+SplayNode* findPred(SplayNode* cur, int x) {
+    if (x >= cur->val) {
+        if (!cur->R) return cur;
+        SplayNode* res = findPred(cur->R, x);
+        if (!res) return cur;
+        return res;
+    } else {
+        if (!cur->L) return NULL;
+        return findPred(cur->L, x);
+    }
+}
+
+SplayNode* findSucc(SplayNode* cur, int x) {
+    if (x <= cur->val) {
+        if (!cur->L) return cur;
+        SplayNode* res = findSucc(cur->L, x);
+        if (!res) return cur;
+        return res;
+    } else {
+        if (!cur->R) return NULL;
+        return findSucc(cur->R, x);
+    }
+}
