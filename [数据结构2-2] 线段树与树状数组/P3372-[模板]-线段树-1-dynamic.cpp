c0edc7e2caad31b14214ded9e16264efc31dd467
Sam <sam@teamzoon.com>
2021-12-03 12:33:31 +0800

    [refactor] refactor P3372 code

--- [数据结构2-2] 线段树与树状数组/P3372-[模板]-线段树-1-dynamic.cpp
+++ [数据结构2-2] 线段树与树状数组/P3372-[模板]-线段树-1-dynamic.cpp
@@ -1,87 +1,74 @@
 #include <cstdio>
 #include <cstdlib>
 
-template <class T>
-class STNode {
-    private:
-        int s, t;
-        T val, tag;
-        STNode *lChild, *rChild;
-
-        void pushDown();
-
-    public:
-        void build(int l, int r);
-        T query(int l, int r);
-        void update(int l, int r, T v);
+struct Node {
+    long long val, tag;
+    int s, t;
+    Node *lChild, *rChild;
 };
 
-template <class T>
-void STNode<T>::build(int l, int r) {
-    this->s = l;
-    this->t = r;
-    this->val = 0;
-    this->tag = 0;
+void build(Node* &cur, int l, int r) {
+    cur->s = l;
+    cur->t = r;
+    cur->val = 0;
+    cur->tag = 0;
     if (l + 1 == r) {
-        this->lChild = this->rChild = NULL;
+        cur->lChild = cur->rChild = NULL;
         return;
     }
-    this->lChild = new STNode;
-    this->rChild = new STNode;
-    this->lChild->build(l, (l + r) >> 1);
-    this->rChild->build((l + r) >> 1, r);
+    cur->lChild = new Node;
+    cur->rChild = new Node;
+    build(cur->lChild, l, (l + r) >> 1);
+    build(cur->rChild, (l + r) >> 1, r);
 }
 
-template <class T>
-void STNode<T>::pushDown() {
-    this->lChild->val += this->tag * (this->lChild->t - this->lChild->s);
-    this->rChild->val += this->tag * (this->rChild->t - this->rChild->s);
-    this->lChild->tag += this->tag;
-    this->rChild->tag += this->tag;
-    this->tag = 0;
+void pushDown(Node* &cur) {
+    cur->lChild->val += cur->tag * (cur->lChild->t - cur->lChild->s);
+    cur->rChild->val += cur->tag * (cur->rChild->t - cur->rChild->s);
+    cur->lChild->tag += cur->tag;
+    cur->rChild->tag += cur->tag;
+    cur->tag = 0;
 }
 
-template <class T>
-T STNode<T>::query(int l, int r) {
-    if (l <= this->s && this->t <= r) return this->val;
-    if (this->tag) this->pushDown();
-    T res = 0;
-    if (l < (this->s + this->t) >> 1) res += this->lChild->query(l, r);
-    if (r > (this->s + this->t) >> 1) res += this->rChild->query(l, r);
+long long query(Node* cur, int l, int r) {
+    if (l <= cur->s && cur->t <= r) return cur->val;
+    if (cur->tag) pushDown(cur);
+    long long res = 0;
+    if (l < (cur->s + cur->t) >> 1) res += query(cur->lChild, l, r);
+    if (r > (cur->s + cur->t) >> 1) res += query(cur->rChild, l, r);
     return res;
 }
 
-template <class T>
-void STNode<T>::update(int l, int r, T v) {
-    if (l <= this->s && this->t <= r) {
-        this->val += v * (this->t - this->s);
-        this->tag += v;
+void update(Node* &cur, int l, int r, long long x) {
+    if (l <= cur->s && cur->t <= r) {
+        cur->val += x * (cur->t - cur->s);
+        cur->tag += x;
         return;
     }
-    if (this->tag) this->pushDown();
-    if (l < (this->s + this->t) >> 1) this->lChild->update(l, r, v);
-    if (r > (this->s + this->t) >> 1) this->rChild->update(l, r, v);
-    this->val = this->lChild->val + this->rChild->val;
+    if (cur->tag) pushDown(cur);
+    if (l < (cur->s + cur->t) >> 1) update(cur->lChild, l, r, x);
+    if (r > (cur->s + cur->t) >> 1) update(cur->rChild, l, r, x);
+    cur->val = cur->lChild->val + cur->rChild->val;
 }
 
 int main() {
     int n, m;
     scanf("%d %d", &n, &m);
-    STNode<long long>* root = new STNode<long long>;
-    root->build(1, n + 1);
+    Node* root = new Node;
+    build(root, 1, n + 1);
     for (int i = 1; i <= n; i++) {
         long long v;
         scanf("%lld", &v);
-        root->update(i, i + 1, v);
+        update(root, i, i + 1, v);
     }
     while (m--) {
         int opt, x, y, k;
         scanf("%d %d %d", &opt, &x, &y);
         if (opt == 1) {
             scanf("%d", &k);
-            root->update(x, y + 1, k);
+            update(root, x, y + 1, k);
         } else {
-            printf("%lld\n", root->query(x, y + 1));
+            printf("%lld\n", query(root, x, y + 1));
         }
     }
     return 0;
