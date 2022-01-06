df751782fa419465d0e8a8aefc94647a2a1afd47
Sam <sam@teamzoon.com>
2022-01-06 11:27:19 +0800

    [update] update P5015 code

--- [入门5] 字符串/P5015-[NOIP2018 普及组]-标题统计.cpp
+++ [入门5] 字符串/P5015-[NOIP2018 普及组]-标题统计.cpp
@@ -3,13 +3,12 @@
 using namespace std;
 
 int main() {
-	int cnt = 0;
-	string s;
-	getline(cin, s);
-	for (int i = 0; i < s.length(); i++) {
-		if (s[i] != ' ' && s[i] != '\n')
-			cnt++;
-	}
-	cout << cnt;
-	return 0;
+    int cnt = 0;
+    string s;
+    getline(cin, s);
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] != ' ' && s[i] != '\n') cnt++;
+    }
+    cout << cnt;
+    return 0;
 }
