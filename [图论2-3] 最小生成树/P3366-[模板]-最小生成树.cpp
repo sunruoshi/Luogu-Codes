7a6b01a99753a9a391362c9839d27a57bc70c845
Sam <sam@teamzoon.com>
2022-03-29 15:43:44 +0800

    [refactor] refactor P3366 code

--- [图论2-3] 最小生成树/P3366-[模板]-最小生成树.cpp
+++ [图论2-3] 最小生成树/P3366-[模板]-最小生成树.cpp
@@ -1,4 +1,4 @@
-#include <cstdio>
+#include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
@@ -7,45 +7,43 @@ struct Edge {
     int u, v, w;
 };
 
-class UnionFind {
-    private:
-        vector<int> fa;
+int n, m, num, ans;
+vector<int> fa;
 
-    public:
-        UnionFind(int n) {
-            fa.resize(n + 1);
-            for (int i = 1; i <= n; i++) {
-                fa[i] = i;
-            }
-        }
-        int Find(int x) {
-            return x == fa[x] ? x : fa[x] = Find(fa[x]);
-        }
-        bool Union(int x, int y) {
-            int xx = Find(x), yy = Find(y);
-            if (xx == yy) return false;
-            fa[xx] = yy;
-            return true;
-        }
-};
+void make_set() {
+    fa.resize(n + 1);
+    for (int i = 1; i <= n; i++) {
+        fa[i] = i;
+    }
+}
+
+int find_set(int x) {
+    return x == fa[x] ? x : fa[x] = find_set(fa[x]);
+}
+
+bool union_set(int x, int y) {
+    int xx = find_set(x), yy = find_set(y);
+    if (xx == yy) return 0;
+    fa[xx] = yy;
+    return 1;
+}
 
 int main() {
-    int n, m, num = 0, ans = 0;
-    scanf("%d %d", &n, &m);
-    UnionFind uf(n);
+    cin >> n >> m;
     vector<Edge> edges(m);
     for (int i = 0; i < m; i++) {
-        scanf("%d %d %d", &edges[i].u, &edges[i].v, &edges[i].w);
+        cin >> edges[i].u >> edges[i].v >> edges[i].w;
     }
+    make_set();
     sort(edges.begin(), edges.end(), [](Edge a, Edge b) { return a.w < b.w; });
     for (auto edge : edges) {
-        if (uf.Union(edge.u, edge.v)) {
+        if (union_set(edge.u, edge.v)) {
             ans += edge.w;
             num++;
         }
         if (num == n - 1) break;
     }
-    if (num != n - 1) printf("orz");
-    else printf("%d", ans);
+    if (num != n - 1) cout << "orz";
+    else cout << ans;
     return 0;
 }
