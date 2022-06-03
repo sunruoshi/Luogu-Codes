bd27544fda0a51253c4e880d6a8cae4999d17fe9
Sam <sam@teamzoon.com>
2022-06-03 15:22:15 +0800

    [update] update P4086 code

--- [算法2-2] 线性复杂度优化及离散化/P4086-[USACO17DEC]-My Cow Ate My Homework-S.cpp
+++ [算法2-2] 线性复杂度优化及离散化/P4086-[USACO17DEC]-My Cow Ate My Homework-S.cpp
@@ -5,7 +5,7 @@ using namespace std;
 const int N = 1e5 + 1;
 
 int n, a[N], s[N];
-double high[N];
+double score[N];
 deque<int> minq;
 
 int main() {
@@ -20,11 +20,11 @@ int main() {
     for (int i = n - 1; i >= 2; i--) {
         while (minq.size() && a[minq.back()] >= a[i]) minq.pop_back();
         minq.push_back(i);
-        high[i - 1] = 1.0 * (s[n] - s[i - 1] - a[minq.front()]) / (n - i);
-        max_score = max(max_score, high[i - 1]);
+        score[i - 1] = 1.0 * (s[n] - s[i - 1] - a[minq.front()]) / (n - i);
+        max_score = max(max_score, score[i - 1]);
     }
     for (int i = 1; i <= n - 2; i++) {
-        if (high[i] == max_score) cout << i << endl;
+        if (score[i] == max_score) cout << i << endl;
     }
     return 0;
 }
