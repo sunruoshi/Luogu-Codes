04d69658e568f5c6552a8e6054b3a6d7db3efbcf
Sam <sam@teamzoon.com>
2022-06-14 12:11:10 +0800

    [update] update P1419 code

--- [算法2-2] 线性复杂度优化及离散化/P1419-寻找段落.cpp
+++ [算法2-2] 线性复杂度优化及离散化/P1419-寻找段落.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 const int N = 1e5 + 1;
+const double EPS = 1e-5;
 
 int n, s, t;
 double a[N], sum[N];
@@ -13,11 +14,15 @@ bool check(double x) {
         sum[i] = sum[i - 1] + a[i] - x;
     }
     deque<int> q;
+    // 对于一个右端点为 i ，左端点为 i - t + 1 到 i - s + 1 的区间都是合法的
+    // 找到所有合法的区间中和最大的区间
+    // 如果最大区间和大于 0 ，则说明还存在更大的答案。
+    // 否则要缩小答案
     for (int i = s; i <= n; i++) {
         while (q.size() && sum[i - s] < sum[q.back()]) q.pop_back();
         q.push_back(i - s);
         while (q.front() < i - t) q.pop_front();
-        if (sum[i] - sum[q.front()] >= 0) return 1;
+        if (sum[i] - sum[q.front()] > EPS) return 1;
     }
     return 0;
 }
@@ -28,8 +33,8 @@ int main() {
     for (int i = 1; i <= n; i++) {
         cin >> a[i];
     }
-    double l = -1e8, r = 1e8;
-    while (r - l > 1e-5) {
+    double l = -1e4, r = 1e4;
+    while (r - l > EPS) {
         double mid = (l + r) / 2;
         if (check(mid)) {
             l = mid;
