89c442400d897a6e0ceae5a4d7803ba6e0bc5d55
sam <sam@2aliens.net>
2022-08-31 16:47:13 +0800

    [update] update P1063 code

--- [OI]/P1063-[NOIP2006-提高组]-能量项链.cpp
+++ [OI]/P1063-[NOIP2006-提高组]-能量项链.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 const int N = 1000;
 
-int n, a[N], dp[N][N], ans;
+int n, a[N], f[N][N], ans;
 
 int main() {
     cin >> n;
@@ -15,11 +15,11 @@ int main() {
     for (int i = (n << 1) - 1; i >= 0; i--) {
         for (int j = i + 1; j <= (n << 1); j++) {
             for (int k = i + 1; k < j; k++) {
-                dp[i][j] = max(dp[i][j], a[i] * a[k] * a[j] + dp[i][k] + dp[k][j]);
+                f[i][j] = max(f[i][j], a[i] * a[k] * a[j] + f[i][k] + f[k][j]);
             }
         }
     }
-    for (int i = 1; i <= n; i++) ans = max(ans, dp[i][n + i]);
+    for (int i = 1; i <= n; i++) ans = max(ans, f[i][n + i]);
     cout << ans << endl;
     return 0;
 }
