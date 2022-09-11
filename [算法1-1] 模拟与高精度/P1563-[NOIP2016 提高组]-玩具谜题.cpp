a60160f54bdf88977b418ca71a8ced39813d763f
sam <sam@2aliens.net>
2022-09-11 10:21:46 +0800

    [update] update P1053 code

--- [算法1-1] 模拟与高精度/P1563-[NOIP2016 提高组]-玩具谜题.cpp
+++ [算法1-1] 模拟与高精度/P1563-[NOIP2016 提高组]-玩具谜题.cpp
@@ -2,25 +2,24 @@
 #include <string>
 using namespace std;
 
+const int N = 1e5;
+
 struct Toy {
     int face;
     string name;
-};
+} t[N];
 
 int main() {
     int n, m, pos = 0;
     cin >> n >> m;
-    Toy toys[n];
-    for (int   i = 0; i < n; i++) {
-        Toy toy;
-        cin >> toy.face >> toy.name;
-        toys[i] = toy;
+    for (int i = 0; i < n; i++) {
+        cin >> t[i].face >> t[i].name;
     }
     for (int i = 0; i < m; i++) {
         int dir, cnt;
         cin >> dir >> cnt;
-        pos = dir ^    toys[pos].face ? (pos + cnt) % n : (pos + n - cnt) % n;
+        pos = dir ^ t[pos].face ? (pos + cnt) % n : (pos + n - cnt) % n;
     }
-    cout <<    toys[pos].name;
+    cout << t[pos].name;
     return 0;
 }
