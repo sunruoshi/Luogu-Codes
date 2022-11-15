efbd04c9d1d915013aba322426d7cee856f1edb9
sam <sam@2aliens.net>
2022-11-15 15:43:20 +0800

    update: update P1966 code

--- [数据结构2-2] 线段树与树状数组/P1966-[NOIP2013 提高组]-火柴排队.cpp
+++ [数据结构2-2] 线段树与树状数组/P1966-[NOIP2013 提高组]-火柴排队.cpp
@@ -2,46 +2,68 @@
 #include <algorithm>
 using namespace std;
 
-const int MAXN = 1e5 + 1, MOD = 1e8 - 3;
+const int N = 1e5 + 1, MOD = 1e8 - 3;
 
-int n, a[MAXN], b[MAXN], c[MAXN], d[MAXN], q[MAXN], bit[MAXN];
+struct Node
+{
+    int val, id;
+    bool operator<(const Node &x) const
+    {
+        return val < x.val;
+    }
+} a[N], b[N];
 
-void update(int x) {
-    while (x <= n) {
-        bit[x] += 1;
-        x += x & (-x);
+int n, id[N], c[N];
+
+inline int lowbit(int x)
+{
+    return x & (-x);
+}
+
+void update(int x)
+{
+    while (x <= n)
+    {
+        c[x] += 1;
+        x += lowbit(x);
     }
 }
 
-int query(int x) {
+int query(int x)
+{
     int res = 0;
-    while (x) {
-        res += bit[x];
-        x -= x & (-x);
+    while (x)
+    {
+        res += c[x];
+        x -= lowbit(x);
     }
     return res;
 }
 
-int main() {
-    cin.sync_with_stdio(0);
+int main()
+{
     cin >> n;
-    for (int i = 1; i <= n; i++) {
-        cin >> a[i];
-        c[i] = i;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i].val;
+        a[i].id = i;
     }
-    for (int i = 1; i <= n; i++) {
-        cin >> b[i];
-        d[i] = i;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> b[i].val;
+        b[i].id = i;
     }
-    sort(c + 1, c + n + 1, [&](int i, int j) { return a[i] < a[j]; });
-    sort(d + 1, d + n + 1, [&](int i, int j) { return b[i] < b[j]; });
-    for (int i = 1; i <= n; i++) {
-        q[c[i]] = d[i];
+    sort(a + 1, a + n + 1);
+    sort(b + 1, b + n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        id[a[i].id] = b[i].id;
     }
     int ans = 0;
-    for (int i = n; i >= 1; i--) {
-        update(q[i]);
-        ans = (ans + query(q[i] - 1)) % MOD;
+    for (int i = n; i >= 1; i--)
+    {
+        update(id[i]);
+        ans = (ans + query(id[i] - 1)) % MOD;
     }
     cout << ans << endl;
     return 0;
